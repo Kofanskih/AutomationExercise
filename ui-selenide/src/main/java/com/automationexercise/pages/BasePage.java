67bcfdd06d629810efd548f9381ab91f89d79978
Kofanskih <kofanskih08@gmail.com>
2025-06-21 11:56:06 +0300

    Add BasePage

---
+++ ui-selenide/src/main/java/com/automationexercise/pages/BasePage.java
@@ -0,0 +1,13 @@
+package com.automationexercise.pages;
+
+import com.codeborne.selenide.Selenide;
+
+import static com.automationexercise.utils.ReadUrl.readMainUrl;
+
+public class BasePage {
+
+    public BasePage open(){
+        Selenide.open(readMainUrl());
+        return new BasePage();
+    }
+}
