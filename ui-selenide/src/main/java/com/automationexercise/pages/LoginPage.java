9e9d45996743c314cc681e6e30372b303ea7032a
Kofanskih <kofanskih08@gmail.com>
2025-06-23 21:58:27 +0300

    Update LoginPage

--- ui-selenide/src/main/java/com/automationexercise/pages/LoginPage.java
+++ ui-selenide/src/main/java/com/automationexercise/pages/LoginPage.java
@@ -10,7 +10,7 @@ public class LoginPage {
     private final SelenideElement EMAIL_ADDRESS_FIELD = $("[data-qa=\"signup-email\"]");
     private final SelenideElement SIGNUP_BUTTON = $("[data-qa=\"signup-button\"]");
 
-    public SignUpPage fillSignUpForm(RegistrationPageModel registrationPageModel){
+    public SignUpPage fillSignUpForm(RegistrationPageModel registrationPageModel) {
         SIGN_UP_NAME_FIELD.setValue(registrationPageModel.getUserFirstName());
         EMAIL_ADDRESS_FIELD.setValue(registrationPageModel.getUserEmailAddress());
         SIGNUP_BUTTON.click();
