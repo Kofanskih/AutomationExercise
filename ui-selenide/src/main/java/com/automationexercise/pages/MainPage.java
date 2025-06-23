685efbcb25f442a5320a43e3fb3d6de1ea98e14b
Kofanskih <kofanskih08@gmail.com>
2025-06-23 22:00:02 +0300

    Update LoginPage

--- ui-selenide/src/main/java/com/automationexercise/pages/MainPage.java
+++ ui-selenide/src/main/java/com/automationexercise/pages/MainPage.java
@@ -1,4 +1,22 @@
 package com.automationexercise.pages;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class MainPage {
+    private final SelenideElement ACCEPT_COOKIES_BUTTON = $("[class=\"fc-button-label\"]");
+    private final SelenideElement USER_LOGGED_IN_TEXT = $("[href=\"/logout\"]");
+
+    public MainPage acceptCookies(){
+        ACCEPT_COOKIES_BUTTON.click();
+        return new MainPage();
+    }
+
+    public MainPage checkUserLoggedIn(String loggedInText){
+        USER_LOGGED_IN_TEXT.shouldHave(Condition.exactTextCaseSensitive(loggedInText));
+        return this;
+    }
+
 }
