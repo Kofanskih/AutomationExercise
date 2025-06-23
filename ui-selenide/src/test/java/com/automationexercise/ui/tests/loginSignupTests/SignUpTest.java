0dd6ee8bffc5f0c0e0569200451b2582eb7629fb
Kofanskih <kofanskih08@gmail.com>
2025-06-23 21:57:02 +0300

    Add SignUpTest

---
+++ ui-selenide/src/test/java/com/automationexercise/ui/tests/loginSignupTests/SignUpTest.java
@@ -0,0 +1,35 @@
+package com.automationexercise.ui.tests.loginSignupTests;
+
+import com.automationexercise.pageModels.RegistrationPageModel;
+import com.automationexercise.pages.BasePage;
+import com.automationexercise.pages.HeaderPage;
+import com.automationexercise.pages.MainPage;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SignUpTest extends BaseTest {
+    private String loggedUser = "Logout";
+
+    @BeforeMethod
+    void preconditionMethod(){
+        new BasePage().open();
+    }
+
+    @Test
+    void userSignUp(){
+        new MainPage().acceptCookies();
+        new HeaderPage()
+                .clickLoginButton()
+                .fillSignUpForm(new RegistrationPageModel().getRandomRegistrationUserData())
+                .chooseGenderFemale()
+                .fillUserData(new RegistrationPageModel().getRandomRegistrationUserData())
+                .chooseCountry()
+                .fillDateOfBirthDropdowns()
+                .markNewsletterCheckbox()
+                .markSpecialOfferCheckbox()
+                .clickCreateAccountButton()
+                .clickContinueButton()
+                .checkUserLoggedIn(loggedUser);
+
+    }
+}
