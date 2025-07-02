3867575f2c58c45fa9f992da3e1837e87add9b91
Kofanskih <kofanskih08@gmail.com>
2025-07-02 16:05:14 +0300

    Update LoginPage

--- ui-selenide/src/main/java/com/automationexercise/pages/LoginPage.java
+++ ui-selenide/src/main/java/com/automationexercise/pages/LoginPage.java
@@ -1,5 +1,6 @@
 package com.automationexercise.pages;
 
+import com.automationexercise.pageModels.LoginPageModel;
 import com.automationexercise.pageModels.RegistrationPageModel;
 import com.codeborne.selenide.SelenideElement;
 
@@ -9,11 +10,23 @@ public class LoginPage {
     private final SelenideElement SIGN_UP_NAME_FIELD = $("[data-qa=\"signup-name\"]");
     private final SelenideElement EMAIL_ADDRESS_FIELD = $("[data-qa=\"signup-email\"]");
     private final SelenideElement SIGNUP_BUTTON = $("[data-qa=\"signup-button\"]");
+    private final SelenideElement LOGIN_EMAIL_ADDRESS_FIELD = $("[data-qa=\"login-email\"]");
+    private final SelenideElement PASSWORD_FIELD = $("[data-qa=\"login-password\"]");
+    private final SelenideElement LOGIN_BUTTON = $("[data-qa=\"login-button\"]");
 
     public SignUpPage fillSignUpForm(RegistrationPageModel registrationPageModel) {
         SIGN_UP_NAME_FIELD.setValue(registrationPageModel.getUserFirstName());
         EMAIL_ADDRESS_FIELD.setValue(registrationPageModel.getUserEmailAddress());
         SIGNUP_BUTTON.click();
+
         return new SignUpPage();
+    }
+
+    public MainPage fillLoginForm(LoginPageModel loginPageModel){
+        LOGIN_EMAIL_ADDRESS_FIELD.setValue(loginPageModel.getUserEmailAddress());
+        PASSWORD_FIELD.setValue(loginPageModel.getUserPassword());
+        LOGIN_BUTTON.click();
+
+        return new MainPage();
     }
 }
