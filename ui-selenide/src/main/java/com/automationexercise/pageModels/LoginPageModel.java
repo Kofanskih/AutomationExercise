fde3f6b699873b3a137347dfbe419be23a638a13
Kofanskih <kofanskih08@gmail.com>
2025-07-02 16:04:39 +0300

    Add LoginPageModel

---
+++ ui-selenide/src/main/java/com/automationexercise/pageModels/LoginPageModel.java
@@ -0,0 +1,21 @@
+package com.automationexercise.pageModels;
+
+import lombok.Data;
+
+@Data
+public class LoginPageModel {
+    private String userEmailAddress;
+    private String userPassword;
+
+    public LoginPageModel myLogin(){
+        this.userEmailAddress = "kofanskih08@gmail.com";
+        this.userPassword = "12345678";
+        return this;
+    }
+
+    public LoginPageModel incorrectDataLogin(){
+        this.userEmailAddress = "kofanskih08@gmail.com";
+        this.userPassword = "1234567";
+        return this;
+    }
+}
