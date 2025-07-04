0493b50e38174f7798973d72d7de51240b479740
Kofanskih <kofanskih08@gmail.com>
2025-07-04 18:10:19 +0300

    Update LoginTest, add userLoginWithEmptyEmailField(), userLoginWithEmptyPasswordField(), userLoginWithInvalidEmail() methods

--- ui-selenide/src/test/java/com/automationexercise/ui/tests/loginTests/LoginTest.java
+++ ui-selenide/src/test/java/com/automationexercise/ui/tests/loginTests/LoginTest.java
@@ -5,12 +5,15 @@ import com.automationexercise.pages.BasePage;
 import com.automationexercise.pages.HeaderPage;
 import com.automationexercise.pages.MainPage;
 import com.automationexercise.ui.tests.BaseTest;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
     private String loggedUser = "Logout";
-    private String errorMessage = "Your email or password is incorrect!";
+    private String loginErrorMessage = "Your email or password is incorrect!";
+    private String emptyInformationMessage = "Заполните это поле.";
+    private String invalidEmailInformationMessage = "Адрес электронной почты должен содержать символ \"@\". В адресе \"kofanskih08gmail.com\" отсутствует символ \"@\".";
 
     @BeforeMethod
     void preconditionMethod(){
@@ -32,6 +35,39 @@ public class LoginTest extends BaseTest {
         new HeaderPage()
                 .clickLoginButton()
                 .fillLoginForm(new LoginPageModel().incorrectDataLogin())
-                .checkLoginErrorMessage(errorMessage);
+                .checkLoginErrorMessage(loginErrorMessage);
     }
+
+    @Test
+    void userLoginWithEmptyEmailField() {
+        new MainPage().acceptCookies();
+        new HeaderPage()
+                .clickLoginButton()
+                .fillLoginFormWithEmptyEmailField(new LoginPageModel().myLogin())
+                .checkShowValidationMessageWhenEmailIsEmpty(emptyInformationMessage);
+    }
+
+    @Test
+    void userLoginWithEmptyPasswordField() {
+        new MainPage().acceptCookies();
+        new HeaderPage()
+                .clickLoginButton()
+                .fillLoginFormWithEmptyPasswordField(new LoginPageModel().myLogin())
+                .checkShowValidationMessageWhenPasswordIsEmpty(emptyInformationMessage);
+    }
+
+    @Test
+    void userLoginWithInvalidEmail() {
+        new MainPage().acceptCookies();
+        new HeaderPage()
+                .clickLoginButton()
+                .fillLoginFormWithInvalidEmail(new LoginPageModel().invalidEmailLogin())
+                .checkShowValidationMessageWhenEmailIsInvalid(invalidEmailInformationMessage);
+    }
+
+    @AfterMethod
+    void postconditionMethod(){
+        new BasePage().close();
+    }
+
 }
