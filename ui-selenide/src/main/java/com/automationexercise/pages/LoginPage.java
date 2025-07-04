46878665a8338692b3c07c8fda59467c263d64a4
Kofanskih <kofanskih08@gmail.com>
2025-07-04 18:08:36 +0300

    Update LoginPage, add checkShowValidationMessageWhenEmailIsEmpty(), fillLoginFormWithEmptyEmailField(), fillLoginFormWithEmptyPasswordField(), checkShowValidationMessageWhenPasswordIsEmpty(), fillLoginFormWithInvalidEmail(), checkShowValidationMessageWhenEmailIsInvalid() methods

--- ui-selenide/src/main/java/com/automationexercise/pages/LoginPage.java
+++ ui-selenide/src/main/java/com/automationexercise/pages/LoginPage.java
@@ -3,20 +3,24 @@ package com.automationexercise.pages;
 import com.automationexercise.pageModels.LoginPageModel;
 import com.automationexercise.pageModels.RegistrationPageModel;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
+import org.openqa.selenium.JavascriptExecutor;
 
 import static com.codeborne.selenide.Selenide.$;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class LoginPage {
     private final SelenideElement SIGN_UP_NAME_FIELD = $("[data-qa=\"signup-name\"]");
-    private final SelenideElement EMAIL_ADDRESS_FIELD = $("[data-qa=\"signup-email\"]");
+    private final SelenideElement SIGNUP_EMAIL_ADDRESS_FIELD = $("[data-qa=\"signup-email\"]");
     private final SelenideElement SIGNUP_BUTTON = $("[data-qa=\"signup-button\"]");
     private final SelenideElement LOGIN_EMAIL_ADDRESS_FIELD = $("[data-qa=\"login-email\"]");
     private final SelenideElement PASSWORD_FIELD = $("[data-qa=\"login-password\"]");
     private final SelenideElement LOGIN_BUTTON = $("[data-qa=\"login-button\"]");
 
+
     public SignUpPage fillSignUpForm(RegistrationPageModel registrationPageModel) {
         SIGN_UP_NAME_FIELD.setValue(registrationPageModel.getUserFirstName());
-        EMAIL_ADDRESS_FIELD.setValue(registrationPageModel.getUserEmailAddress());
+        SIGNUP_EMAIL_ADDRESS_FIELD.setValue(registrationPageModel.getUserEmailAddress());
         SIGNUP_BUTTON.click();
 
         return new SignUpPage();
@@ -32,9 +36,54 @@ public class LoginPage {
 
     public SignUpPage fillSignUpFormWithExistsUser(RegistrationPageModel registrationPageModel) {
         SIGN_UP_NAME_FIELD.setValue(registrationPageModel.getUserName());
-        EMAIL_ADDRESS_FIELD.setValue(registrationPageModel.getUserEmailAddress());
+        SIGNUP_EMAIL_ADDRESS_FIELD.setValue(registrationPageModel.getUserEmailAddress());
         SIGNUP_BUTTON.click();
 
         return new SignUpPage();
     }
+
+    public LoginPage fillLoginFormWithEmptyEmailField(LoginPageModel loginPageModel){
+        LOGIN_EMAIL_ADDRESS_FIELD.clear();
+        PASSWORD_FIELD.setValue(loginPageModel.getUserPassword());
+        LOGIN_BUTTON.click();
+
+        return new LoginPage();
+    }
+
+
+    public void checkShowValidationMessageWhenEmailIsEmpty(String expectedInformationMessage) {
+        String actualMessage = (String) ((JavascriptExecutor) WebDriverRunner.getWebDriver())
+                .executeScript("return arguments[0].validationMessage;", LOGIN_EMAIL_ADDRESS_FIELD);
+        assertEquals(actualMessage, expectedInformationMessage);
+    }
+
+    public LoginPage fillLoginFormWithEmptyPasswordField(LoginPageModel loginPageModel){
+        LOGIN_EMAIL_ADDRESS_FIELD.setValue(loginPageModel.getUserEmailAddress());
+        PASSWORD_FIELD.clear();
+        LOGIN_BUTTON.click();
+
+        return new LoginPage();
+    }
+
+    public void checkShowValidationMessageWhenPasswordIsEmpty(String expectedInformationMessage) {
+        String actualMessage = (String) ((JavascriptExecutor) WebDriverRunner.getWebDriver())
+                .executeScript("return arguments[0].validationMessage;", PASSWORD_FIELD);
+        assertEquals(actualMessage, expectedInformationMessage);
+    }
+
+
+    public LoginPage fillLoginFormWithInvalidEmail(LoginPageModel loginPageModel){
+        LOGIN_EMAIL_ADDRESS_FIELD.setValue(loginPageModel.getUserEmailAddress());
+        PASSWORD_FIELD.setValue(loginPageModel.getUserPassword());
+        LOGIN_BUTTON.click();
+
+        return new LoginPage();
+    }
+
+    public void checkShowValidationMessageWhenEmailIsInvalid(String expectedInformationMessage) {
+        String actualMessage = (String) ((JavascriptExecutor) WebDriverRunner.getWebDriver())
+                .executeScript("return arguments[0].validationMessage;", LOGIN_EMAIL_ADDRESS_FIELD);
+        assertEquals(actualMessage, expectedInformationMessage);
+    }
+
 }
