d9e0ce4f3a8fda2597798e2766f226aea23b4572
Kofanskih <kofanskih08@gmail.com>
2025-07-07 17:13:54 +0300

    Update ProductsPages, add closeAddedModalWindowOnTheProductsPage(), checkModalWindowIsClosedOnTheProductPage() methods

--- ui-selenide/src/main/java/com/automationexercise/pages/ProductsPage.java
+++ ui-selenide/src/main/java/com/automationexercise/pages/ProductsPage.java
@@ -12,6 +12,9 @@ public class ProductsPage extends BasePage {
     private final SelenideElement SEARCH_RESULT = $x("//p[contains(., 'Winter Top')]");
     private final SelenideElement ADD_ITEM_TO_CART = $("[data-product-id=\"3\"]");
     private final SelenideElement ADDED_MODAL_WINDOW = $("[class=\"modal-title w-100\"]");
+    private final SelenideElement CONTINUE_SHOPPING_BUTTON = $("[class=\"btn btn-success close-modal btn-block\"]");
+    private final SelenideElement MODAL_WINDOW_HIDDEN = $("[class=\"modal fade\"]");
+    private final SelenideElement VIEW_PRODUCT_BUTTON = $x("(//i[@class='fa fa-plus-square'])[2]");
 
     public ProductsPage searchItemOnTheProductsPage(String searchedItem){
         SEARCH_FIELD.setValue(searchedItem);
@@ -24,12 +27,21 @@ public class ProductsPage extends BasePage {
     }
 
     public ProductsPage addItemToTheCartOnTheProductsPage(){
-        ADD_ITEM_TO_CART.click();
+        ADD_ITEM_TO_CART.scrollTo().click();
         return this;
     }
 
     public void checkAddedItemOnTheProductPage(String infoModalMessage){
         ADDED_MODAL_WINDOW.shouldHave(Condition.exactTextCaseSensitive(infoModalMessage));
+    }
+
+    public ProductsPage closeAddedModalWindowOnTheProductsPage(){
+        CONTINUE_SHOPPING_BUTTON.click();
+        return this;
+    }
+
+    public void checkModalWindowIsClosedOnTheProductPage(){
+        MODAL_WINDOW_HIDDEN.exists();
     }
 
 
