4b1f54a92a19601256b5c946f8587fdb63581b16
Kofanskih <kofanskih08@gmail.com>
2025-07-07 21:52:44 +0300

    Update CartPage, add checkUserOnTheCartPage() method

--- ui-selenide/src/main/java/com/automationexercise/pages/CartPage.java
+++ ui-selenide/src/main/java/com/automationexercise/pages/CartPage.java
@@ -1,4 +1,14 @@
 package com.automationexercise.pages;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class CartPage {
+    private final SelenideElement TEXT_TITLE = $("[class=\"active\"]");
+
+    public void checkUserOnTheCartPage(String title){
+        TEXT_TITLE.shouldHave(Condition.exactTextCaseSensitive(title));
+    }
 }
