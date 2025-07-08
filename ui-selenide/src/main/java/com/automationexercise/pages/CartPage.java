09a35b1b44ac3dbc036b0fa9abf13f264976f1b4
Kofanskih <kofanskih08@gmail.com>
2025-07-08 16:41:16 +0300

    Update CartPage, add checkTheCartIsEmpty(), clickHereButtonTheCartPage() methods

--- ui-selenide/src/main/java/com/automationexercise/pages/CartPage.java
+++ ui-selenide/src/main/java/com/automationexercise/pages/CartPage.java
@@ -4,11 +4,24 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 
 public class CartPage {
     private final SelenideElement TEXT_TITLE = $("[class=\"active\"]");
+    private final SelenideElement EMPTY_TITLE = $("[id=\"empty_cart\"]");
+    private final SelenideElement HERE_BUTTON = $x("//u[contains(., 'here')]");
 
-    public void checkUserOnTheCartPage(String title){
+    public void checkUserOnTheCartPage(String title) {
         TEXT_TITLE.shouldHave(Condition.exactTextCaseSensitive(title));
     }
+
+    public void checkTheCartIsEmpty(String title) {
+        EMPTY_TITLE.shouldHave(Condition.exactTextCaseSensitive(title));
+    }
+
+    public CartPage clickHereButtonTheCartPage(){
+        HERE_BUTTON.click();
+        return this;
+    }
+
 }
