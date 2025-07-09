2d1e908807117df8d40144b17c4b00cf830c0b42
Kofanskih <kofanskih08@gmail.com>
2025-07-09 22:06:59 +0300

    Update MainPage, add checkUrlOnTheMainPage(), addItemToTheCartOnTheMainPage(), checkAddedItemOnTheMainPage() methods

--- ui-selenide/src/main/java/com/automationexercise/pages/MainPage.java
+++ ui-selenide/src/main/java/com/automationexercise/pages/MainPage.java
@@ -2,17 +2,21 @@ package com.automationexercise.pages;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
 
 import static com.codeborne.selenide.Selenide.$;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class MainPage {
     private final SelenideElement ACCEPT_COOKIES_BUTTON = $("[class=\"fc-button-label\"]");
     private final SelenideElement USER_LOGGED_IN_TEXT = $("[href=\"/logout\"]");
     private final SelenideElement LOGIN_ERROR_TEXT = $("[style=\"color: red;\"]");
+    private final SelenideElement ADD_ITEM_TO_CART = $("[data-product-id=\"3\"]");
+    private final SelenideElement ADDED_MODAL_WINDOW = $("[class=\"modal-title w-100\"]");
 
-    public void acceptCookies(){
+    public MainPage acceptCookies(){
         ACCEPT_COOKIES_BUTTON.click();
-        new MainPage();
+        return new MainPage();
     }
 
     public void checkUserLoggedIn(String loggedInText){
@@ -23,5 +27,18 @@ public class MainPage {
         LOGIN_ERROR_TEXT.shouldHave(Condition.exactTextCaseSensitive(loginErrorMessage));
     }
 
+    public void checkUrlOnTheMainPage(String expectedUrl){
+        String actualUrl = WebDriverRunner.url();
+        assertEquals(expectedUrl, actualUrl);
+    }
+
+    public MainPage addItemToTheCartOnTheMainPage(){
+        ADD_ITEM_TO_CART.scrollTo().click();
+        return this;
+    }
+
+    public void checkAddedItemOnTheMainPage(String infoModalMessage){
+        ADDED_MODAL_WINDOW.shouldHave(Condition.exactTextCaseSensitive(infoModalMessage));
+    }
 
 }
