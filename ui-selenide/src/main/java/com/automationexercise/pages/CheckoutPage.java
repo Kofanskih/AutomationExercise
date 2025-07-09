e6d645eca871899d4082768392097e693cfccb6d
Kofanskih <kofanskih08@gmail.com>
2025-07-09 20:12:40 +0300

    Update CheckoutPage, add goToTheLoginPage() method

--- ui-selenide/src/main/java/com/automationexercise/pages/CheckoutPage.java
+++ ui-selenide/src/main/java/com/automationexercise/pages/CheckoutPage.java
@@ -1,13 +1,21 @@
 package com.automationexercise.pages;
 
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 
+import static com.codeborne.selenide.Selenide.$x;
 import static org.testng.AssertJUnit.assertEquals;
 
 public class CheckoutPage {
+    private final SelenideElement REGISTER_LOGIN_BUTTON = $x("//p//a[@href=\"/login\"]");
 
     public void checkUrlOnTheCheckoutPage(String expectedUrl) throws InterruptedException {
         String actualUrl = WebDriverRunner.url();
         assertEquals(expectedUrl, actualUrl);
+    }
+
+    public LoginPage goToTheLoginPage(){
+        REGISTER_LOGIN_BUTTON.click();
+        return new LoginPage();
     }
 }
