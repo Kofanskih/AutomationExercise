d4ebbf4494c91ff002886999fd5ba2bc9ec8b302
Kofanskih <kofanskih08@gmail.com>
2025-07-11 11:33:25 +0300

    Updated TestCasesPage, added checkTitleOnTheTestCasesPage() method

--- ui-selenide/src/main/java/com/automationexercise/pages/TestCasesPage.java
+++ ui-selenide/src/main/java/com/automationexercise/pages/TestCasesPage.java
@@ -1,4 +1,14 @@
 package com.automationexercise.pages;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class TestCasesPage {
+    private final SelenideElement CENTER_TEXT_TITLE = $("[class=\"title text-center\"]");
+
+    public void checkTitleOnTheTestCasesPage(String title){
+        CENTER_TEXT_TITLE.shouldHave(Condition.exactTextCaseSensitive(title));
+    }
 }
