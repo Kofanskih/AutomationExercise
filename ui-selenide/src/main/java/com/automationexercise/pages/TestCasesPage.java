bc46d44decd96b3c47a9cf2a36b101f28b076101
Anastasiia Bukovtseva <44721588+Kofanskih@users.noreply.github.com>
2025-07-11 21:04:01 +0300

    Test cases
    * Added TestCasesTests
    
    * Updated TestCasesPage, added expandCollapsedList(), checkTheListIsExpanded(), collapseExpandedList(), checkTheListIsCollapsed() tests

--- ui-selenide/src/main/java/com/automationexercise/pages/TestCasesPage.java
+++ ui-selenide/src/main/java/com/automationexercise/pages/TestCasesPage.java
@@ -7,8 +7,30 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class TestCasesPage {
     private final SelenideElement CENTER_TEXT_TITLE = $("[class=\"title text-center\"]");
+    private final SelenideElement LIST = $("[href=\"#collapse1\"]");
+    private final SelenideElement EXPANDED_BLOCK = $("[class=\"panel-collapse in\"]");
+    private final SelenideElement COLLAPSED_BLOCK = $("[class=\"collapsed\"]");
 
     public void checkTitleOnTheTestCasesPage(String title){
         CENTER_TEXT_TITLE.shouldHave(Condition.exactTextCaseSensitive(title));
+    }
+
+    public TestCasesPage expandCollapsedList(){
+        LIST.click();
+        return this;
+    }
+
+    public void checkTheListIsExpanded(){
+        EXPANDED_BLOCK.shouldBe(Condition.appear);
+    }
+
+    public TestCasesPage collapseExpandedList(){
+        EXPANDED_BLOCK.shouldBe(Condition.appear);
+        LIST.click();
+        return this;
+    }
+
+    public void checkTheListIsCollapsed(){
+        COLLAPSED_BLOCK.shouldBe(Condition.appear);
     }
 }
