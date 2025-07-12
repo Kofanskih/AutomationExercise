2f2449c92d6e9b9c307116fd9be6096feb132b01
Kofanskih <kofanskih08@gmail.com>
2025-07-12 13:57:21 +0300

    Updated DeletePage, added checkTitleAfterDeletingAccount(), clickContinueButtonOnTheDeletePage() methods

--- ui-selenide/src/main/java/com/automationexercise/pages/DeletePage.java
+++ ui-selenide/src/main/java/com/automationexercise/pages/DeletePage.java
@@ -1,4 +1,20 @@
 package com.automationexercise.pages;
 
-public class DeletePage {
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class DeletePage extends BasePage{
+    private final SelenideElement ACCOUNT_DELETED_TITLE = $("[data-qa=\"account-deleted\"]");
+    private final SelenideElement CONTINUE_BUTTON = $("[data-qa=\"continue-button\"]");
+
+    public void checkTitleAfterDeletingAccount(String title){
+        ACCOUNT_DELETED_TITLE.shouldHave(Condition.exactTextCaseSensitive(title));
+    }
+
+    public MainPage clickContinueButtonOnTheDeletePage(){
+        CONTINUE_BUTTON.click();
+        return new MainPage();
+    }
 }
