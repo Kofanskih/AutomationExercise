58bd28ba7cd2e24c9430613b6367dc71be6a91fe
Kofanskih <kofanskih08@gmail.com>
2025-07-13 15:23:09 +0300

    Updated CartPage, small fixes

--- ui-selenide/src/main/java/com/automationexercise/pages/CartPage.java
+++ ui-selenide/src/main/java/com/automationexercise/pages/CartPage.java
@@ -7,14 +7,14 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class CartPage extends BasePage{
-    private final SelenideElement TEXT_TITLE = $("[class=\"active\"]");
+    private final SelenideElement TITLE = $("[class=\"active\"]");
     private final SelenideElement EMPTY_TITLE = $("[id=\"empty_cart\"]");
     private final SelenideElement HERE_BUTTON = $x("//u[contains(., 'here')]");
     private final SelenideElement DELETE_BUTTON = $("[class=\"cart_quantity_delete\"]");
     private final SelenideElement PROCEED_TO_CHECKOUT_BUTTON = $x("//div//a[@class=\"btn btn-default check_out\"]");
 
     public void checkUserOnTheCartPage(String title) {
-        TEXT_TITLE.shouldHave(Condition.exactTextCaseSensitive(title));
+        TITLE.shouldHave(Condition.exactTextCaseSensitive(title));
     }
 
     public void checkTheCartIsEmpty(String title) {
