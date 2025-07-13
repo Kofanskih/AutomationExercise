0f34f91df86f0fc3dc4f63821171bb12b4222852
Anastasiia Bukovtseva <44721588+Kofanskih@users.noreply.github.com>
2025-07-13 13:53:33 +0300

    Video tutorials
    * Updated VideoTutorialsPage, added checkUrlOnThePage()
    
    * Added VideoTutorialsTests

--- ui-selenide/src/main/java/com/automationexercise/pages/VideoTutorialsPage.java
+++ ui-selenide/src/main/java/com/automationexercise/pages/VideoTutorialsPage.java
@@ -1,4 +1,13 @@
 package com.automationexercise.pages;
 
+import com.codeborne.selenide.WebDriverRunner;
+
+import static org.testng.AssertJUnit.assertEquals;
+
 public class VideoTutorialsPage extends BasePage{
+
+    public void checkUrlOnThePage(String expectedUrl){
+        String actualUrl = WebDriverRunner.url();
+        assertEquals(expectedUrl, actualUrl);
+    }
 }
