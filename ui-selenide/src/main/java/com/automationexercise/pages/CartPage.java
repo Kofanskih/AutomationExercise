c87f769c9810dfcd158ee070469cd79f951e4ed4
Kofanskih <kofanskih08@gmail.com>
2025-07-15 11:37:18 +0300

    Changes in CartPage, updated checkTheCartIsEmpty(),

--- ui-selenide/src/main/java/com/automationexercise/pages/CartPage.java
+++ ui-selenide/src/main/java/com/automationexercise/pages/CartPage.java
@@ -3,14 +3,16 @@ package com.automationexercise.pages;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class CartPage extends BasePage{
     private final SelenideElement TITLE = $("[class=\"active\"]");
-    private final SelenideElement EMPTY_TITLE = $("[id=\"empty_cart\"]");
+    private final SelenideElement EMPTY_TITLE = $x("//b[contains(., 'Cart is empty!')]");
     private final SelenideElement HERE_BUTTON = $x("//u[contains(., 'here')]");
-    private final SelenideElement DELETE_BUTTON = $("[class=\"cart_quantity_delete\"]");
+    private final SelenideElement DELETE_BUTTON = $x("//a[@class=\"cart_quantity_delete\"]");
     private final SelenideElement PROCEED_TO_CHECKOUT_BUTTON = $x("//div//a[@class=\"btn btn-default check_out\"]");
 
     public void checkUserOnTheCartPage(String title) {
@@ -18,7 +20,8 @@ public class CartPage extends BasePage{
     }
 
     public void checkTheCartIsEmpty(String title) {
-        EMPTY_TITLE.shouldHave(Condition.exactTextCaseSensitive(title));
+        EMPTY_TITLE.shouldBe(Condition.visible, Duration.ofSeconds(1))
+                .shouldHave(Condition.exactTextCaseSensitive(title));
     }
 
     public ProductsPage clickHereButtonTheCartPage(){
@@ -35,6 +38,4 @@ public class CartPage extends BasePage{
         PROCEED_TO_CHECKOUT_BUTTON.click();
         return new CheckoutPage();
     }
-
-
 }
