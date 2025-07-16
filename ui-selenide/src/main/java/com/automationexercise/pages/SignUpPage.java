a15c8989303ec0b4836313bc02dbf861fefad28f
Kofanskih <kofanskih08@gmail.com>
2025-07-16 20:12:15 +0300

    Updated SignUpPage, added allure step annotations

--- ui-selenide/src/main/java/com/automationexercise/pages/SignUpPage.java
+++ ui-selenide/src/main/java/com/automationexercise/pages/SignUpPage.java
@@ -3,6 +3,7 @@ package com.automationexercise.pages;
 import com.automationexercise.pageModels.RegistrationPageModel;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -32,12 +33,13 @@ public class SignUpPage extends BasePage{
     private final SelenideElement CONTINUE_BUTTON = $("[data-qa=\"continue-button\"]");
     private final SelenideElement SIGNUP_ERROR_MESSAGE = $("[style=\"color: red;\"]");
 
-
+    @Step("Choose female gender")
     public SignUpPage chooseGenderFemale(){
         FEMALE_RADIOBUTTON.click();
         return this;
     }
 
+    @Step("Fill sugn up form")
     public SignUpPage fillUserData(RegistrationPageModel registrationPageModel) {
         PASSWORD_FIELD.setValue(registrationPageModel.getUserPassword());
         FIRST_NAME_FIELD.setValue(registrationPageModel.getUserFirstName());
@@ -52,12 +54,14 @@ public class SignUpPage extends BasePage{
         return this;
     }
 
+    @Step("Choose country")
     public SignUpPage chooseCountry(){
         COUNTRY_DROPDOWN.click();
         COUNTRY_VALUE.click();
         return this;
     }
 
+    @Step("Fill date of birth dropdowns")
     public SignUpPage fillDateOfBirthDropdowns(){
         DAYS_DROPDOWN.click();
         DAY_VALUE.click();
@@ -68,26 +72,31 @@ public class SignUpPage extends BasePage{
         return this;
     }
 
+    @Step("Mark newsletter checkbox")
     public SignUpPage markNewsletterCheckbox(){
         NEWSLETTER_CHECKBOX.click();
         return this;
     }
 
+    @Step("Mark special offer checkbox")
     public SignUpPage markSpecialOfferCheckbox(){
         SPECIAL_OFFER_CHECKBOX.click();
         return this;
     }
 
+    @Step("Click create account button")
     public SignUpPage clickCreateAccountButton() {
         CREATE_ACCOUNT_BUTTON.scrollTo().click();
         return this;
     }
 
+    @Step("Click continue account button")
     public MainPage clickContinueButton(){
         CONTINUE_BUTTON.click();
         return new MainPage();
     }
 
+    @Step("Check Sign up error message")
     public void checkSignUpErrorMessage(String signUpErrorMessage){
         SIGNUP_ERROR_MESSAGE.shouldHave(Condition.exactTextCaseSensitive(signUpErrorMessage));
     }
