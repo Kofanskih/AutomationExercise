d4bd281379617bf66516a4a15ae7f97cb7434676
Kofanskih <kofanskih08@gmail.com>
2025-07-16 15:10:09 +0300

    Updated FooterPage, added allure step annotations

--- ui-selenide/src/main/java/com/automationexercise/pages/FooterPage.java
+++ ui-selenide/src/main/java/com/automationexercise/pages/FooterPage.java
@@ -3,6 +3,7 @@ package com.automationexercise.pages;
 import com.automationexercise.pageModels.RegistrationPageModel;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Step;
 import org.openqa.selenium.JavascriptExecutor;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -14,7 +15,7 @@ public class FooterPage extends BasePage{
     private final SelenideElement SUBSCRIBE_BUTTON = $("[id=\"subscribe\"]");
     private final SelenideElement SUCCESS_SUBSCRIBE_MESSAGE = $x("//div[@class=\"alert-success alert\"]");
 
-
+    @Step("Check that user is subscribed")
     public void checkUserIsSubscribed(RegistrationPageModel registrationPageModel, String message){
         SUBSCRIBE_FIELD.setValue(registrationPageModel.getUserEmailAddress());
         SUBSCRIBE_BUTTON.click();
@@ -22,11 +23,13 @@ public class FooterPage extends BasePage{
         assertEquals(message, actualText);
     }
 
+    @Step("Fill subscribe field with invalid email")
     public FooterPage fillSubscribeFieldWithInvalidEmail(RegistrationPageModel registrationPageModel){
         SUBSCRIBE_FIELD.setValue(registrationPageModel.getUserEmailAddress());
         return this;
     }
 
+    @Step("Check validation message if user fill invalid email")
     public void checkShowValidationMessageUsingInvalidEmailForSubscription(String message) {
         String actualMessage = (String) ((JavascriptExecutor) WebDriverRunner.getWebDriver())
                 .executeScript("return arguments[0].validationMessage;", SUBSCRIBE_FIELD);
