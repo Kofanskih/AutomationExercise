79566ebe1208c23f1c8079ae630c195928f1abfb
Kofanskih <kofanskih08@gmail.com>
2025-07-16 16:27:49 +0300

    Updated DeletePage, added allure step annotations

--- ui-selenide/src/main/java/com/automationexercise/pages/DeletePage.java
+++ ui-selenide/src/main/java/com/automationexercise/pages/DeletePage.java
@@ -2,6 +2,7 @@ package com.automationexercise.pages;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -9,10 +10,12 @@ public class DeletePage extends BasePage{
     private final SelenideElement ACCOUNT_DELETED_TITLE = $("[data-qa=\"account-deleted\"]");
     private final SelenideElement CONTINUE_BUTTON = $("[data-qa=\"continue-button\"]");
 
+    @Step("Check the title after deleting account")
     public void checkTitleAfterDeletingAccount(String title){
         ACCOUNT_DELETED_TITLE.shouldHave(Condition.exactTextCaseSensitive(title));
     }
 
+    @Step("Click Continue button on the Delete page")
     public MainPage clickContinueButtonOnTheDeletePage(){
         CONTINUE_BUTTON.click();
         return new MainPage();
