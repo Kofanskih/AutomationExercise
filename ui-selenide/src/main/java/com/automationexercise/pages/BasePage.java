66a676efccc9bae35a72384fffd189316e1ce1ac
Kofanskih <kofanskih08@gmail.com>
2025-07-16 16:01:30 +0300

    Updated BasePage, added allure step annotations

--- ui-selenide/src/main/java/com/automationexercise/pages/BasePage.java
+++ ui-selenide/src/main/java/com/automationexercise/pages/BasePage.java
@@ -1,15 +1,19 @@
 package com.automationexercise.pages;
 
 import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Step;
+
 import static com.automationexercise.utils.ReadUrl.readMainUrl;
 
 public class BasePage {
 
+    @Step("Open main url")
     public BasePage open(){
         Selenide.open(readMainUrl());
         return new BasePage();
     }
 
+    @Step("Close Webdriver")
     public void close(){
         Selenide.closeWebDriver();
     }
