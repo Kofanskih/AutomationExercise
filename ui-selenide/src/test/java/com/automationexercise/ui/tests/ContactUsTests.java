933b1e4c6b14804d02af0039a410903aef7d3a2c
Kofanskih <kofanskih08@gmail.com>
2025-07-17 18:49:36 +0300

    Updated ContactUsTests, added test descriptions, allure annotations

--- ui-selenide/src/test/java/com/automationexercise/ui/tests/ContactUsTests.java
+++ ui-selenide/src/test/java/com/automationexercise/ui/tests/ContactUsTests.java
@@ -5,10 +5,17 @@ import com.automationexercise.pages.BasePage;
 import com.automationexercise.pages.HeaderPage;
 import com.automationexercise.pages.MainPage;
 import com.automationexercise.utils.ConfigurateBrowserSettings;
+import io.qameta.allure.*;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+@Epic("Feedback")
+@Feature("Contact us")
+@Story("Feedback")
+@Severity(SeverityLevel.NORMAL)
+@Owner("bukovtseva")
+@TmsLink("TC-008")
 public class ContactUsTests {
     private String successfulMessage = "Success! Your details have been submitted successfully.";
     private String expectedMainURL = "https://automationexercise.com/";
