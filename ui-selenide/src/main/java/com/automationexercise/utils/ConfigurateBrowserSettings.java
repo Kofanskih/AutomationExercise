42d63fc9bbe0def0d569cc932705ac19fac1deb6
Kofanskih <kofanskih08@gmail.com>
2025-07-17 21:22:18 +0300

    Updated ConfigurateBrowserSettings, add screenshot functionality to the setUp() method

--- ui-selenide/src/main/java/com/automationexercise/utils/ConfigurateBrowserSettings.java
+++ ui-selenide/src/main/java/com/automationexercise/utils/ConfigurateBrowserSettings.java
@@ -1,6 +1,8 @@
 package com.automationexercise.utils;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 
 public class ConfigurateBrowserSettings {
     public void setUp(){
@@ -8,5 +10,9 @@ public class ConfigurateBrowserSettings {
         Configuration.browser = "chrome";
         Configuration.timeout = 5;
         Configuration.headless = true;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(false));
     }
 }
