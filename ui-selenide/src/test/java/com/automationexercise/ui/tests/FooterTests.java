fb97d463641254cd61c0c25e9613e7ce6b901fe2
Kofanskih <kofanskih08@gmail.com>
2025-07-17 18:48:10 +0300

    Updated FooterTests, added test descriptions, allure annotations

--- ui-selenide/src/test/java/com/automationexercise/ui/tests/FooterTests.java
+++ ui-selenide/src/test/java/com/automationexercise/ui/tests/FooterTests.java
@@ -5,10 +5,17 @@ import com.automationexercise.pages.BasePage;
 import com.automationexercise.pages.FooterPage;
 import com.automationexercise.pages.MainPage;
 import com.automationexercise.utils.ConfigurateBrowserSettings;
+import io.qameta.allure.*;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+@Epic("Subscription")
+@Feature("User subscription")
+@Story("Subscription")
+@Severity(SeverityLevel.NORMAL)
+@Owner("bukovtseva")
+@TmsLink("TC-007")
 public class FooterTests {
     private String successfullSubscriptionValidationMessage = "You have been successfully subscribed!";
     private String errorValidationMessage = "Адрес электронной почты должен содержать символ \"@\". В адресе \"kofanskih08gmail.com\" отсутствует символ \"@\".";
