59c53d6c6700591c425149afb2ccfd6b42075b5e
Kofanskih <kofanskih08@gmail.com>
2025-07-17 18:52:42 +0300

    Updated VideoTutorialsTests, added test descriptions, allure annotations

--- ui-selenide/src/test/java/com/automationexercise/ui/tests/VideoTutorialsTests.java
+++ ui-selenide/src/test/java/com/automationexercise/ui/tests/VideoTutorialsTests.java
@@ -4,10 +4,17 @@ import com.automationexercise.pages.BasePage;
 import com.automationexercise.pages.HeaderPage;
 import com.automationexercise.pages.MainPage;
 import com.automationexercise.utils.ConfigurateBrowserSettings;
+import io.qameta.allure.*;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+@Epic("Video Tutorials")
+@Feature("Video Tutorials")
+@Story("Video Tutorials")
+@Severity(SeverityLevel.MINOR)
+@Owner("bukovtseva")
+@TmsLink("TC-011")
 public class VideoTutorialsTests {
     private String expectedURL = "https://consent.youtube.com/m?continue=https%3A%2F%2Fwww.youtube.com%2Fc%2FAutomationExercise%3Fcbrd%3D1&gl=ES&m=0&pc=yt&cm=2&hl=ru&src=1";
 
