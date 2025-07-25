dd1f34815c613d7eada46cb1cfea4da75a45853c
Kofanskih <kofanskih08@gmail.com>
2025-07-25 16:13:22 +0300

    Updated AssertableResponse, added checkHtmlResponseContainsValue

--- api-restassured/src/main/java/com/automationexercise/conditions/AssertableResponse.java
+++ api-restassured/src/main/java/com/automationexercise/conditions/AssertableResponse.java
@@ -1,10 +1,17 @@
 package com.automationexercise.conditions;
 
 import io.qameta.allure.Step;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.testng.Assert;
+
+import java.io.IOException;
+import java.util.List;
 
 @AllArgsConstructor
 @Slf4j
@@ -26,4 +33,17 @@ public class AssertableResponse {
         }
         return this;
     }
+
+    public <T> void checkHtmlResponseContainsValue(String jsonPathExpr, T expectedValue) {
+        String html = response.getBody().asString();
+        Document doc = Jsoup.parse(html);
+        String jsonText = doc.body().wholeText();
+
+        JsonPath jsonPath = new JsonPath(jsonText);
+        List<T> values = jsonPath.getList(jsonPathExpr);
+
+        Assert.assertTrue(values.contains(expectedValue),
+                "Value '" + expectedValue + "' was not found in the list: " + values);
+    }
+
 }
