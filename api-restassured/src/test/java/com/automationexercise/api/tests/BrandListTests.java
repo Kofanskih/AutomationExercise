0c5558cdd658f80035f8e9a8cb7253bab9df190b
Kofanskih <kofanskih08@gmail.com>
2025-07-26 17:02:03 +0300

    Updated BrandListTests, added verifyResponseCodeInPutRequestForBrandsList(), verifyMessageInPuttRequestForBrandsList()

--- api-restassured/src/test/java/com/automationexercise/api/tests/BrandListTests.java
+++ api-restassured/src/test/java/com/automationexercise/api/tests/BrandListTests.java
@@ -13,11 +13,16 @@ public class BrandListTests {
     private final int BRAND_ID = 8;
     private final String PATH_BRAND = "brands.brand";
     private final String BRAND = "H&M";
+    private final String PATH_RESPONSE_CODE = "responseCode";
+    private final int RESPONSE_CODE = 405;
+    private final String PATH_MESSAGE = "message";
+    private final String MESSAGE = "This request method is not supported.";
 
 
     @Test
     public void verifyBrandsList() throws IOException {
-        new BrandListService().sendGetBrandListRequest()
+        new BrandListService()
+                .sendGetBrandListRequest()
                 .shouldHave(statusCode(200))
                 .shouldHave(contentType("text/html"));
     }
@@ -25,17 +30,33 @@ public class BrandListTests {
     @Test
     public void checkHtmlResponseContainsBrandId() throws IOException {
         new BrandListService()
-                .sendGetBrandListRequest().shouldHave(statusCode(200))
+                .sendGetBrandListRequest()
+                .shouldHave(statusCode(200))
                 .checkHtmlResponseContainsValueInTheList(PATH_ID, BRAND_ID);
     }
 
     @Test
     public void checkHtmlResponseContainsBrand() throws IOException {
         new BrandListService()
-                .sendGetBrandListRequest().shouldHave(statusCode(200))
+                .sendGetBrandListRequest()
+                .shouldHave(statusCode(200))
                 .checkHtmlResponseContainsValueInTheList(PATH_BRAND, BRAND);
     }
 
+    @Test
+    public void verifyResponseCodeInPutRequestForBrandsList() throws IOException {
+        new BrandListService()
+                .sendPutBrandListRequest()
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE);
+    }
 
+    @Test
+    public void verifyMessageInPuttRequestForBrandsList() throws IOException {
+        new BrandListService()
+                .sendPutBrandListRequest()
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_MESSAGE, MESSAGE);
+    }
 
 }
