44bb7a6cf2a57a37dcc160e6fe4c13bcb8426b8a
Kofanskih <kofanskih08@gmail.com>
2025-07-27 19:58:01 +0300

    Updated CreateAccountTests, added verifyCreateUserCorrectResponseCode()

--- api-restassured/src/test/java/com/automationexercise/api/tests/CreateAccountTests.java
+++ api-restassured/src/test/java/com/automationexercise/api/tests/CreateAccountTests.java
@@ -10,9 +10,10 @@ import java.util.Map;
 import static com.automationexercise.conditions.Conditions.statusCode;
 
 public class CreateAccountTests {
-
     private final String PATH_MESSAGE = "message";
     private final String USER_CREATED_MESSAGE = "User created!";
+    private final String PATH_RESPONSE_CODE = "responseCode";
+    private final int RESPONSE_CODE_200 = 201;
 
     @Test
     public void verifyCreateUserCorrectInfoMessage() throws IOException {
@@ -24,5 +25,14 @@ public class CreateAccountTests {
                 .checkHtmlResponseContainsValue(PATH_MESSAGE, USER_CREATED_MESSAGE);
     }
 
+    @Test
+    public void verifyCreateUserCorrectResponseCode() throws IOException {
+        CreateUserModel user = new CreateUserModel().getRandomCreateUserData();
+        Map<String, String> userData = user.toMap();
+        new CreateAccountService()
+                .sendPostCreateUserRequest(userData)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE_200);
+    }
 
 }
