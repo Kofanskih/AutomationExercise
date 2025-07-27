440a3b3db85595e2371c88a54b202b48bdd38fe9
Kofanskih <kofanskih08@gmail.com>
2025-07-27 13:28:51 +0300

    Updated SearchProductTests, added verifyResponseCodeInTheEmptySearch(), verifyMessageInPutRequestInTheEmptySearch()

--- api-restassured/src/test/java/com/automationexercise/api/tests/SearchProductTests.java
+++ api-restassured/src/test/java/com/automationexercise/api/tests/SearchProductTests.java
@@ -10,6 +10,10 @@ import static com.automationexercise.conditions.Conditions.statusCode;
 public class SearchProductTests {
     private final String SEARCH_PATH = "products.name";
     private final String SEARCH_REQUEST = "TOP";
+    private final String PATH_RESPONSE_CODE = "responseCode";
+    private final int RESPONSE_CODE = 400;
+    private final String PATH_MESSAGE = "message";
+    private final String MESSAGE = "Bad request, search_product parameter is missing in POST request.";
 
     @Test
     public void verifyCorrectSearch() throws IOException {
@@ -17,6 +21,22 @@ public class SearchProductTests {
                 .sendPostSearchProductRequest(SEARCH_REQUEST)
                 .shouldHave(statusCode(200))
                 .checkHtmlResponseContainsSearchedValueInTheList(SEARCH_PATH, SEARCH_REQUEST);
+    }
+
+    @Test
+    public void verifyResponseCodeInTheEmptySearch() throws IOException {
+        new SearchProductService()
+                .sendPostEmptySearchRequest()
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE);
+    }
+
+    @Test
+    public void verifyMessageInPutRequestInTheEmptySearch() throws IOException {
+        new SearchProductService()
+                .sendPostEmptySearchRequest()
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_MESSAGE, MESSAGE);
     }
 
 }
