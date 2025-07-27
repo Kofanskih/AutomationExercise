97eb7a398898a66720b2d764e173378e3654acb7
Kofanskih <kofanskih08@gmail.com>
2025-07-27 21:36:51 +0300

    Added DeleteAccountTests, added createUserForDeleting(), verifyCorrectDeleteAccountResponseCode()

---
+++ api-restassured/src/test/java/com/automationexercise/api/tests/DeleteAccountTests.java
@@ -0,0 +1,34 @@
+package com.automationexercise.api.tests;
+
+import com.automationexercise.models.createUser.CreateUserModel;
+import com.automationexercise.service.CreateAccountService;
+import com.automationexercise.service.DeleteAccountService;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static com.automationexercise.conditions.Conditions.statusCode;
+
+public class DeleteAccountTests {
+    private final String EMAIL = "test123test123test123@test.com";
+    private final String PASSWORD = "12345678";
+    private final String PATH_RESPONSE_CODE = "responseCode";
+    private final int RESPONSE_CODE_200 = 200;
+
+    @BeforeMethod
+    public void createUserForDeleting() throws IOException{
+        CreateUserModel user = new CreateUserModel().getCreateUserDataForDeletingAccount();
+        Map<String, String> userData = user.toMap();
+        new CreateAccountService()
+                .sendPostCreateUserRequest(userData);
+    }
+
+    @Test
+    public void verifyCorrectDeleteAccountResponseCode() throws IOException {
+        new DeleteAccountService().sendDeleteAccountRequest(EMAIL, PASSWORD)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE_200);
+    }
+}
