b8a24cc93f1e5ace804d0997e115e245505d0a41
Kofanskih <kofanskih08@gmail.com>
2025-07-27 12:41:55 +0300

    Updated GetPath, made methods as static

--- api-restassured/src/main/java/utils/GetPath.java
+++ api-restassured/src/main/java/utils/GetPath.java
@@ -6,19 +6,17 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class GetPath {
-    private final String PATH = "src/main/resources/paths.properties";
+    private static final String PATH = "src/main/resources/paths.properties";
 
-    private Properties readPath() throws IOException {
+    private static Properties readPath() throws IOException {
         Properties props = new Properties();
         InputStream input = new FileInputStream(PATH);
         props.load(input);
         return props;
     }
 
-    public String getPath(String keyProperty) throws IOException {
+    public static String getPath(String keyProperty) throws IOException {
         String key = readPath().getProperty(keyProperty);
         return key;
     }
-
-
 }
