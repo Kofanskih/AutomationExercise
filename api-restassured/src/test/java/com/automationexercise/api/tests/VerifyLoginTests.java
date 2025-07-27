d9cd2aaa26556799c34bff4e83266b708ec9cfde
Kofanskih <kofanskih08@gmail.com>
2025-07-27 16:16:24 +0300

    Updated VerifyLoginTests, added verifyCorrectLoginMessageOnlyWithEmailParam(), verifyCorrectLoginResponseCodeOnlyWithEmailParam()

--- api-restassured/src/test/java/com/automationexercise/api/tests/VerifyLoginTests.java
+++ api-restassured/src/test/java/com/automationexercise/api/tests/VerifyLoginTests.java
@@ -15,9 +15,11 @@ public class VerifyLoginTests {
     private final String PATH_RESPONSE_CODE = "responseCode";
     private final int RESPONSE_CODE_200 = 200;
     private final int RESPONSE_CODE_404 = 404;
+    private final int RESPONSE_CODE_400 = 400;
     private final String PATH_MESSAGE = "message";
     private final String USER_EXISTS_MESSAGE = "User exists!";
     private final String USER_NOT_FOUND_MESSAGE = "User not found!";
+    private final String BAD_REQUEST_MESSAGE = "Bad request, email or password parameter is missing in POST request.";
 
 
     @Test
@@ -37,7 +39,7 @@ public class VerifyLoginTests {
     }
 
     @Test
-    public void verifyInCorrectLoginMessageWithInvalidEmail() throws IOException {
+    public void verifyCorrectLoginMessageWithInvalidEmail() throws IOException {
         new VerifyLoginService()
                 .sendPostVerifyLoginRequest(INCORRECT_LOGIN, CORRECT_PASSWORD)
                 .shouldHave(statusCode(200))
@@ -45,7 +47,7 @@ public class VerifyLoginTests {
     }
 
     @Test
-    public void verifyInCorrectLoginResponseCodeWithInvalidEmail() throws IOException {
+    public void verifyCorrectLoginResponseCodeWithInvalidEmail() throws IOException {
         new VerifyLoginService()
                 .sendPostVerifyLoginRequest(INCORRECT_LOGIN, CORRECT_PASSWORD)
                 .shouldHave(statusCode(200))
@@ -53,7 +55,7 @@ public class VerifyLoginTests {
     }
 
     @Test
-    public void verifyInCorrectLoginMessageWithInvalidPassword() throws IOException {
+    public void verifyCorrectMessageLoginWithInvalidPassword() throws IOException {
         new VerifyLoginService()
                 .sendPostVerifyLoginRequest(CORRECT_LOGIN, INCORRECT_PASSWORD)
                 .shouldHave(statusCode(200))
@@ -61,10 +63,26 @@ public class VerifyLoginTests {
     }
 
     @Test
-    public void verifyInCorrectLoginResponseCodeWithInvalidPassword() throws IOException {
+    public void verifyCorrectLoginResponseCodeWithInvalidPassword() throws IOException {
         new VerifyLoginService()
                 .sendPostVerifyLoginRequest(CORRECT_LOGIN, INCORRECT_PASSWORD)
                 .shouldHave(statusCode(200))
                 .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE_404);
+    }
+
+    @Test
+    public void verifyCorrectLoginMessageOnlyWithEmailParam() throws IOException {
+        new VerifyLoginService()
+                .sendPostVerifyLoginRequestOnlyWithEmailParam(CORRECT_LOGIN)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_MESSAGE, BAD_REQUEST_MESSAGE);
+    }
+
+    @Test
+    public void verifyCorrectLoginResponseCodeOnlyWithEmailParam() throws IOException {
+        new VerifyLoginService()
+                .sendPostVerifyLoginRequestOnlyWithEmailParam(CORRECT_LOGIN)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE_400);
     }
 }
