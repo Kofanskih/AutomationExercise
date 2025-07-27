2cb5e275419b187310873685509cff6c06129ba6
Kofanskih <kofanskih08@gmail.com>
2025-07-27 15:47:32 +0300

    Updated VerifyLoginTests, added verifyInCorrectLoginMessageWithInvalidPassword(), verifyInCorrectLoginResponseCodeWithInvalidPassword()

--- api-restassured/src/test/java/com/automationexercise/api/tests/VerifyLoginTests.java
+++ api-restassured/src/test/java/com/automationexercise/api/tests/VerifyLoginTests.java
@@ -11,6 +11,7 @@ public class VerifyLoginTests {
     private final String CORRECT_LOGIN = "1test1test@test.com";
     private final String CORRECT_PASSWORD = "12345678";
     private final String INCORRECT_LOGIN = "11test1test@test.com";
+    private final String INCORRECT_PASSWORD = "123456789";
     private final String PATH_RESPONSE_CODE = "responseCode";
     private final int RESPONSE_CODE_200 = 200;
     private final int RESPONSE_CODE_404 = 404;
@@ -47,6 +48,22 @@ public class VerifyLoginTests {
     public void verifyInCorrectLoginResponseCodeWithInvalidEmail() throws IOException {
         new VerifyLoginService()
                 .sendPostVerifyLoginRequest(INCORRECT_LOGIN, CORRECT_PASSWORD)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE_404);
+    }
+
+    @Test
+    public void verifyInCorrectLoginMessageWithInvalidPassword() throws IOException {
+        new VerifyLoginService()
+                .sendPostVerifyLoginRequest(CORRECT_LOGIN, INCORRECT_PASSWORD)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_MESSAGE, USER_NOT_FOUND_MESSAGE);
+    }
+
+    @Test
+    public void verifyInCorrectLoginResponseCodeWithInvalidPassword() throws IOException {
+        new VerifyLoginService()
+                .sendPostVerifyLoginRequest(CORRECT_LOGIN, INCORRECT_PASSWORD)
                 .shouldHave(statusCode(200))
                 .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE_404);
     }
