98f8885d65ebadad32a02dbab1dcf713d4ea841b
Kofanskih <kofanskih08@gmail.com>
2025-07-27 12:40:21 +0300

    Added SearchProductService, added sendPostSearchProductRequest()

---
+++ api-restassured/src/main/java/com/automationexercise/service/SearchProductService.java
@@ -0,0 +1,24 @@
+package com.automationexercise.service;
+
+import com.automationexercise.conditions.AssertableResponse;
+import io.restassured.response.Response;
+
+import java.io.IOException;
+
+import static utils.GetPath.getPath;
+
+public class SearchProductService extends BaseService {
+    private final String SEARCH_PRODUCT_PATH = "search.product";
+    private final String PARAM = "search_product";
+    private final String X_WWW_FORM_URLENCODED = "application/x-www-form-urlencoded";
+
+    public AssertableResponse sendPostSearchProductRequest(String searchRequest) throws IOException {
+        Response response =
+                baseConfiguration()
+                        .contentType(X_WWW_FORM_URLENCODED)
+                        .formParam(PARAM, searchRequest)
+                        .post(getPath(SEARCH_PRODUCT_PATH))
+                        .then().extract().response();
+        return new AssertableResponse(response);
+    }
+}
