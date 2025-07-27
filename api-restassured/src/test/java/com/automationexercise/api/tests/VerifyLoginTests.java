dbffa275be90dc632c33308d3c78372988fae493
Kofanskih <kofanskih08@gmail.com>
2025-07-27 15:40:01 +0300

    Updated VerifyLoginTests, added verifyInCorrectLoginMessageWithInvalidEmail(), verifyInCorrectLoginResponseCodeWithInvalidEmail()

--- api-restassured/src/test/java/com/automationexercise/api/tests/VerifyLoginTests.java
+++ api-restassured/src/test/java/com/automationexercise/api/tests/VerifyLoginTests.java
@@ -10,10 +10,13 @@ import static com.automationexercise.conditions.Conditions.statusCode;
 public class VerifyLoginTests {
     private final String CORRECT_LOGIN = "1test1test@test.com";
     private final String CORRECT_PASSWORD = "12345678";
+    private final String INCORRECT_LOGIN = "11test1test@test.com";
     private final String PATH_RESPONSE_CODE = "responseCode";
-    private final int RESPONSE_CODE = 200;
+    private final int RESPONSE_CODE_200 = 200;
+    private final int RESPONSE_CODE_404 = 404;
     private final String PATH_MESSAGE = "message";
-    private final String MESSAGE = "User exists!";
+    private final String USER_EXISTS_MESSAGE = "User exists!";
+    private final String USER_NOT_FOUND_MESSAGE = "User not found!";
 
 
     @Test
@@ -21,7 +24,7 @@ public class VerifyLoginTests {
         new VerifyLoginService()
                 .sendPostVerifyLoginRequest(CORRECT_LOGIN, CORRECT_PASSWORD)
                 .shouldHave(statusCode(200))
-                .checkHtmlResponseContainsValue(PATH_MESSAGE, MESSAGE);
+                .checkHtmlResponseContainsValue(PATH_MESSAGE, USER_EXISTS_MESSAGE);
     }
 
     @Test
@@ -29,6 +32,22 @@ public class VerifyLoginTests {
         new VerifyLoginService()
                 .sendPostVerifyLoginRequest(CORRECT_LOGIN, CORRECT_PASSWORD)
                 .shouldHave(statusCode(200))
-                .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE);
+                .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE_200);
+    }
+
+    @Test
+    public void verifyInCorrectLoginMessageWithInvalidEmail() throws IOException {
+        new VerifyLoginService()
+                .sendPostVerifyLoginRequest(INCORRECT_LOGIN, CORRECT_PASSWORD)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_MESSAGE, USER_NOT_FOUND_MESSAGE);
+    }
+
+    @Test
+    public void verifyInCorrectLoginResponseCodeWithInvalidEmail() throws IOException {
+        new VerifyLoginService()
+                .sendPostVerifyLoginRequest(INCORRECT_LOGIN, CORRECT_PASSWORD)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE_404);
     }
 }
