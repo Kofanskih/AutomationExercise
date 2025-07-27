d7cc501bc15df4a073957ec4b010e7ed04d967da
Anastasiia Bukovtseva <44721588+Kofanskih@users.noreply.github.com>
2025-07-27 16:59:32 +0300

    Verify login
    * Added VerifyLoginService, added sendPostVerifyLoginRequest()
    
    * Added VerifyLoginTests, added verifyCorrectLoginMessage(), verifyCorrectLoginResponseCode()
    
    * Updated VerifyLoginTests, added verifyInCorrectLoginMessageWithInvalidEmail(), verifyInCorrectLoginResponseCodeWithInvalidEmail()
    
    * Updated VerifyLoginTests, added verifyInCorrectLoginMessageWithInvalidPassword(), verifyInCorrectLoginResponseCodeWithInvalidPassword()
    
    * Updated VerifyLoginTests, added verifyCorrectLoginMessageOnlyWithEmailParam(), verifyCorrectLoginResponseCodeOnlyWithEmailParam()
    
    * Updated VerifyLoginService, added sendPostVerifyLoginRequestOnlyWithEmailParam()
    
    * Updated VerifyLoginService, added sendPostVerifyLoginRequestOnlyWithPasswordParam()
    
    * Updated VerifyLoginTests, added verifyCorrectLoginMessageOnlyWithPasswordParam(), verifyCorrectLoginResponseCodeOnlyWithPasswordParam()
    
    * Updated VerifyLoginService, added sendDeleteVerifyLoginRequest()
    
    * Updated VerifyLoginTests, renamed tests, added verifyCorrectInfoMessageForLoginWithDeleteMethod(), verifyCorrectResponseCodeForLoginWithDeleteMethod()

---
+++ api-restassured/src/main/java/com/automationexercise/service/VerifyLoginService.java
@@ -0,0 +1,54 @@
+package com.automationexercise.service;
+
+import com.automationexercise.conditions.AssertableResponse;
+import io.restassured.response.Response;
+
+import java.io.IOException;
+
+import static utils.GetPath.getPath;
+
+public class VerifyLoginService extends BaseService {
+    private final String VERIFY_LOGIN_PATH = "verify.login";
+    private final String EMAIL_PARAM = "email";
+    private final String PASSWORD_PARAM = "password";
+    private final String X_WWW_FORM_URLENCODED = "application/x-www-form-urlencoded";
+
+    public AssertableResponse sendPostVerifyLoginRequest(String email, String password) throws IOException {
+        Response response =
+                baseConfiguration()
+                        .contentType(X_WWW_FORM_URLENCODED)
+                        .formParams(EMAIL_PARAM, email, PASSWORD_PARAM, password)
+                        .post(getPath(VERIFY_LOGIN_PATH))
+                        .then().extract().response();
+        return new AssertableResponse(response);
+    }
+
+    public AssertableResponse sendPostVerifyLoginRequestOnlyWithEmailParam(String email) throws IOException {
+        Response response =
+                baseConfiguration()
+                        .contentType(X_WWW_FORM_URLENCODED)
+                        .formParam(EMAIL_PARAM, email)
+                        .post(getPath(VERIFY_LOGIN_PATH))
+                        .then().extract().response();
+        return new AssertableResponse(response);
+    }
+
+    public AssertableResponse sendPostVerifyLoginRequestOnlyWithPasswordParam(String password) throws IOException {
+        Response response =
+                baseConfiguration()
+                        .contentType(X_WWW_FORM_URLENCODED)
+                        .formParam(PASSWORD_PARAM, password)
+                        .post(getPath(VERIFY_LOGIN_PATH))
+                        .then().extract().response();
+        return new AssertableResponse(response);
+    }
+
+    public AssertableResponse sendDeleteVerifyLoginRequest() throws IOException {
+        Response response =
+                baseConfiguration()
+                        .contentType(X_WWW_FORM_URLENCODED)
+                        .delete(getPath(VERIFY_LOGIN_PATH))
+                        .then().extract().response();
+        return new AssertableResponse(response);
+    }
+}
---
+++ api-restassured/src/test/java/com/automationexercise/api/tests/VerifyLoginTests.java
@@ -0,0 +1,122 @@
+package com.automationexercise.api.tests;
+
+import com.automationexercise.service.VerifyLoginService;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static com.automationexercise.conditions.Conditions.statusCode;
+
+public class VerifyLoginTests {
+    private final String CORRECT_LOGIN = "1test1test@test.com";
+    private final String CORRECT_PASSWORD = "12345678";
+    private final String INCORRECT_LOGIN = "11test1test@test.com";
+    private final String INCORRECT_PASSWORD = "123456789";
+    private final String PATH_RESPONSE_CODE = "responseCode";
+    private final int RESPONSE_CODE_200 = 200;
+    private final int RESPONSE_CODE_404 = 404;
+    private final int RESPONSE_CODE_400 = 400;
+    private final int RESPONSE_CODE_405 = 405;
+    private final String PATH_MESSAGE = "message";
+    private final String USER_EXISTS_MESSAGE = "User exists!";
+    private final String USER_NOT_FOUND_MESSAGE = "User not found!";
+    private final String BAD_REQUEST_MESSAGE = "Bad request, email or password parameter is missing in POST request.";
+    private final String NOT_SUPPORTED_MESSAGE = "This request method is not supported.";
+
+
+    @Test
+    public void verifyCorrectLoginInfoMessage() throws IOException {
+        new VerifyLoginService()
+                .sendPostVerifyLoginRequest(CORRECT_LOGIN, CORRECT_PASSWORD)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_MESSAGE, USER_EXISTS_MESSAGE);
+    }
+
+    @Test
+    public void verifyCorrectLoginResponseCode() throws IOException {
+        new VerifyLoginService()
+                .sendPostVerifyLoginRequest(CORRECT_LOGIN, CORRECT_PASSWORD)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE_200);
+    }
+
+    @Test
+    public void verifyCorrectInfoMessageWithInvalidEmailLogin() throws IOException {
+        new VerifyLoginService()
+                .sendPostVerifyLoginRequest(INCORRECT_LOGIN, CORRECT_PASSWORD)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_MESSAGE, USER_NOT_FOUND_MESSAGE);
+    }
+
+    @Test
+    public void verifyCorrectResponseCodeWithInvalidEmailLogin() throws IOException {
+        new VerifyLoginService()
+                .sendPostVerifyLoginRequest(INCORRECT_LOGIN, CORRECT_PASSWORD)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE_404);
+    }
+
+    @Test
+    public void verifyCorrectInfoMessageWithInvalidPasswordLogin() throws IOException {
+        new VerifyLoginService()
+                .sendPostVerifyLoginRequest(CORRECT_LOGIN, INCORRECT_PASSWORD)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_MESSAGE, USER_NOT_FOUND_MESSAGE);
+    }
+
+    @Test
+    public void verifyCorrectResponseCodeWithInvalidPasswordLogin() throws IOException {
+        new VerifyLoginService()
+                .sendPostVerifyLoginRequest(CORRECT_LOGIN, INCORRECT_PASSWORD)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE_404);
+    }
+
+    @Test
+    public void verifyCorrectInfoMessageOnlyWithEmailParamLogin() throws IOException {
+        new VerifyLoginService()
+                .sendPostVerifyLoginRequestOnlyWithEmailParam(CORRECT_LOGIN)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_MESSAGE, BAD_REQUEST_MESSAGE);
+    }
+
+    @Test
+    public void verifyCorrectLoginResponseCodeOnlyWithEmailParamLogin() throws IOException {
+        new VerifyLoginService()
+                .sendPostVerifyLoginRequestOnlyWithEmailParam(CORRECT_LOGIN)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE_400);
+    }
+
+    @Test
+    public void verifyCorrectInfoMessageOnlyWithPasswordParamLogin() throws IOException {
+        new VerifyLoginService()
+                .sendPostVerifyLoginRequestOnlyWithPasswordParam(CORRECT_PASSWORD)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_MESSAGE, BAD_REQUEST_MESSAGE);
+    }
+
+    @Test
+    public void verifyCorrectResponseCodeOnlyWithPasswordParamLogin() throws IOException {
+        new VerifyLoginService()
+                .sendPostVerifyLoginRequestOnlyWithPasswordParam(CORRECT_PASSWORD)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE_400);
+    }
+
+    @Test
+    public void verifyCorrectInfoMessageForLoginWithDeleteMethod() throws IOException {
+        new VerifyLoginService()
+                .sendDeleteVerifyLoginRequest()
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_MESSAGE, NOT_SUPPORTED_MESSAGE);
+    }
+
+    @Test
+    public void verifyCorrectResponseCodeForLoginWithDeleteMethod() throws IOException {
+        new VerifyLoginService()
+                .sendDeleteVerifyLoginRequest()
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE_405);
+    }
+}
