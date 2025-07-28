667d7e66e320c97c93c4886d2d5321bbf159ffff
Kofanskih <kofanskih08@gmail.com>
2025-07-28 20:23:53 +0300

    Updated GetUserDetailByEmailTests, added verifyCorrectInfoMessageForNotExistUser(), verifyCorrectResponseCodeForNotExistUser()

--- api-restassured/src/test/java/com/automationexercise/api/tests/GetUserDetailByEmailTests.java
+++ api-restassured/src/test/java/com/automationexercise/api/tests/GetUserDetailByEmailTests.java
@@ -11,11 +11,15 @@ import static com.automationexercise.conditions.Conditions.statusCode;
 public class GetUserDetailByEmailTests  {
     private final String PATH_EMAIL = "user.email";
     private final String EXIST_USER_EMAIL = "test1234567890test@test.com";
+    private final String NOT_EXIST_USER_EMAIL = "1test1234567890test@test.com";
     private final String PATH_RESPONSE_CODE = "responseCode";
-    private final int RESPONSE_CODE_200 = 20;
+    private final int RESPONSE_CODE_200 = 200;
+    private final int RESPONSE_CODE_404 = 404;
+    private final String PATH_MESSAGE = "message";
+    private final String ACCOUNT_NOT_FOUND_INFO_MESSAGE = "Account not found with this email, try another email!";
 
     @Test
-    public void verifyProductList() throws IOException {
+    public void verifyGetUserDetailByEmail() throws IOException {
         new GetUserDetailByEmailService().sendGetUserDetailByEmailRequest(EXIST_USER_EMAIL)
                 .shouldHave(statusCode(200)).shouldHave(contentType("text/html"));
     }
@@ -34,5 +38,21 @@ public class GetUserDetailByEmailTests  {
                 .sendGetUserDetailByEmailRequest(EXIST_USER_EMAIL)
                 .shouldHave(statusCode(200))
                 .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE_200);
+    }
+
+    @Test
+    public void verifyCorrectInfoMessageForNotExistUser() throws IOException {
+        new GetUserDetailByEmailService()
+                .sendGetUserDetailByEmailRequest(NOT_EXIST_USER_EMAIL)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_MESSAGE, ACCOUNT_NOT_FOUND_INFO_MESSAGE);
+    }
+
+    @Test
+    public void verifyCorrectResponseCodeForNotExistUser() throws IOException {
+        new GetUserDetailByEmailService()
+                .sendGetUserDetailByEmailRequest(NOT_EXIST_USER_EMAIL)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE_404);
     }
 }
