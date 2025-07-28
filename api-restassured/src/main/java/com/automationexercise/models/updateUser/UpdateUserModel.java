ac8ff54f454d638dee31b5339e710e0598804957
Anastasiia Bukovtseva <44721588+Kofanskih@users.noreply.github.com>
2025-07-28 16:44:00 +0300

    Update account
    * Added UpdateUserModel, added getRandomUpdateExistsUserData(), toMap()
    
    * Added UpdateAccountService, added sendPutUpdateUserRequest()
    
    * Added UpdateAccountTests, added verifyUpdateUserCorrectInfoMessage()
    
    * Updated UpdateAccountTests, added verifyUpdateUserCorrectResponseCode()
    
    * Updated UpdateUserModel, added getRandomUpdateNotExistsUserData()
    
    * Updated UpdateAccountTests, added verifyUpdateNotExistUserCorrectInfoMessage()
    
    * Updated UpdateAccountTests, added verifyUpdateNotExistUserCorrectResponseCode()

---
+++ api-restassured/src/main/java/com/automationexercise/models/updateUser/UpdateUserModel.java
@@ -0,0 +1,94 @@
+package com.automationexercise.models.updateUser;
+
+import lombok.Data;
+import net.datafaker.Faker;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+public class UpdateUserModel {
+    private Faker user = new Faker();
+    private String userName;
+    private String userEmailAddress;
+    private String userPassword;
+    private String userTitle;
+    private String userBirthDate;
+    private String userBirthMonth;
+    private String userBirthYear;
+    private String userFirstName;
+    private String userLastName;
+    private String userCompany;
+    private String userAddress;
+    private String userAddress2;
+    private String userCountry;
+    private String userState;
+    private String userCity;
+    private String userZipcode;
+    private String userMobile;
+
+    public UpdateUserModel getRandomUpdateExistsUserData(){
+        this.userName = user.name().username();
+        this.userEmailAddress = "test1234567890test@test.com";
+        this.userPassword = "12345678";
+        this.userTitle = "Mr";
+        this.userBirthDate = String.valueOf(user.number().numberBetween(1, 28));
+        this.userBirthMonth = String.valueOf(user.number().numberBetween(1, 12));
+        this.userBirthYear = String.valueOf(user.number().numberBetween(1970, 2000));
+        this.userFirstName = user.name().firstName();
+        this.userLastName = user.name().lastName();
+        this.userCompany = user.company().name();
+        this.userAddress = user.address().streetAddress();
+        this.userAddress2 = user.address().secondaryAddress();
+        this.userCountry = "Canada";
+        this.userZipcode = user.address().zipCode();
+        this.userState = user.address().state();
+        this.userCity = user.address().city();
+        this.userMobile = user.phoneNumber().cellPhone();
+        return this;
+    }
+
+    public UpdateUserModel getRandomUpdateNotExistsUserData(){
+        this.userName = user.name().username();
+        this.userEmailAddress = "1234567890test1234567890test@test.com";
+        this.userPassword = "12345678";
+        this.userTitle = "Mr";
+        this.userBirthDate = String.valueOf(user.number().numberBetween(1, 28));
+        this.userBirthMonth = String.valueOf(user.number().numberBetween(1, 12));
+        this.userBirthYear = String.valueOf(user.number().numberBetween(1970, 2000));
+        this.userFirstName = user.name().firstName();
+        this.userLastName = user.name().lastName();
+        this.userCompany = user.company().name();
+        this.userAddress = user.address().streetAddress();
+        this.userAddress2 = user.address().secondaryAddress();
+        this.userCountry = "Canada";
+        this.userZipcode = user.address().zipCode();
+        this.userState = user.address().state();
+        this.userCity = user.address().city();
+        this.userMobile = user.phoneNumber().cellPhone();
+        return this;
+    }
+
+    public Map<String, String> toMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", userName);
+        map.put("email", userEmailAddress);
+        map.put("password", userPassword);
+        map.put("title", userTitle);
+        map.put("birth_date", userBirthDate);
+        map.put("birth_month", userBirthMonth);
+        map.put("birth_year", userBirthYear);
+        map.put("firstname", userFirstName);
+        map.put("lastname", userLastName);
+        map.put("company", userCompany);
+        map.put("address1", userAddress);
+        map.put("address2", userAddress2);
+        map.put("country", userCountry);
+        map.put("zipcode", userZipcode);
+        map.put("state", userState);
+        map.put("city", userCity);
+        map.put("mobile_number", userMobile);
+        return map;
+    }
+
+}
---
+++ api-restassured/src/test/java/com/automationexercise/api/tests/UpdateAccountTests.java
@@ -0,0 +1,61 @@
+package com.automationexercise.api.tests;
+
+import com.automationexercise.models.updateUser.UpdateUserModel;
+import com.automationexercise.service.UpdateAccountService;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static com.automationexercise.conditions.Conditions.statusCode;
+
+public class UpdateAccountTests {
+    private final String PATH_MESSAGE = "message";
+    private final String USER_UPDATED_MESSAGE = "User updated!";
+    private final String ACCOUNT_NOT_FOUND_MESSAGE = "Account not found!";
+    private final String PATH_RESPONSE_CODE = "responseCode";
+    private final int RESPONSE_CODE_200 = 200;
+    private final int RESPONSE_CODE_404 = 404;
+
+
+    @Test
+    public void verifyUpdateUserCorrectInfoMessage() throws IOException {
+        UpdateUserModel user = new UpdateUserModel().getRandomUpdateExistsUserData();
+        Map<String, String> userData = user.toMap();
+        new UpdateAccountService()
+                .sendPutUpdateUserRequest(userData)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_MESSAGE, USER_UPDATED_MESSAGE);
+    }
+
+    @Test
+    public void verifyUpdateUserCorrectResponseCode() throws IOException {
+        UpdateUserModel user = new UpdateUserModel().getRandomUpdateExistsUserData();
+        Map<String, String> userData = user.toMap();
+        new UpdateAccountService()
+                .sendPutUpdateUserRequest(userData)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE_200);
+    }
+
+    @Test
+    public void verifyUpdateNotExistUserCorrectInfoMessage() throws IOException {
+        UpdateUserModel user = new UpdateUserModel().getRandomUpdateNotExistsUserData();
+        Map<String, String> userData = user.toMap();
+        new UpdateAccountService()
+                .sendPutUpdateUserRequest(userData)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_MESSAGE, ACCOUNT_NOT_FOUND_MESSAGE);
+    }
+
+    @Test
+    public void verifyUpdateNotExistUserCorrectResponseCode() throws IOException {
+        UpdateUserModel user = new UpdateUserModel().getRandomUpdateNotExistsUserData();
+        Map<String, String> userData = user.toMap();
+        new UpdateAccountService()
+                .sendPutUpdateUserRequest(userData)
+                .shouldHave(statusCode(200))
+                .checkHtmlResponseContainsValue(PATH_RESPONSE_CODE, RESPONSE_CODE_404);
+    }
+
+}
