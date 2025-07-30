54bb1a6147d09525232010b4d18a253b02d48713
Kofanskih <kofanskih08@gmail.com>
2025-07-30 22:52:28 +0300

    Added allure annotations to the GetUserDetailByEmailService

--- api-restassured/src/main/java/com/automationexercise/service/GetUserDetailByEmailService.java
+++ api-restassured/src/main/java/com/automationexercise/service/GetUserDetailByEmailService.java
@@ -1,6 +1,7 @@
 package com.automationexercise.service;
 
 import com.automationexercise.conditions.AssertableResponse;
+import io.qameta.allure.Step;
 import io.restassured.response.Response;
 
 import java.io.IOException;
@@ -11,6 +12,7 @@ public class GetUserDetailByEmailService extends BaseService{
     private final String GET_USER_DETAIL_BY_EMAIL_PATH = "get.user.detail.by.email";
     private final String EMAIL_PARAM = "email";
 
+    @Step("API send get user detail by email request")
     public AssertableResponse sendGetUserDetailByEmailRequest(String email) throws IOException {
         Response response =
                 baseConfiguration()
