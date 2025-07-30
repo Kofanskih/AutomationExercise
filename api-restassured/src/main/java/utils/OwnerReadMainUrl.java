3f283b93f3463dc5e6aae5137af3b4e1bed5c937
Kofanskih <kofanskih08@gmail.com>
2025-07-30 23:05:29 +0300

    Added allure annotations to the OwnerReadMainUrl

--- api-restassured/src/main/java/utils/OwnerReadMainUrl.java
+++ api-restassured/src/main/java/utils/OwnerReadMainUrl.java
@@ -1,10 +1,12 @@
 package utils;
 
+import io.qameta.allure.Step;
 import org.aeonbits.owner.ConfigFactory;
 
 public class OwnerReadMainUrl {
     public static OwnerConfig config = ConfigFactory.create(OwnerConfig.class);
 
+    @Step("API Read main URL from properties")
     public static String readMainUrl(){
         return config.mainUrl();
     }
