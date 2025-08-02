d28d8f056bad0cec2d59cf565fe6c2b5bce6ce7e
Kofanskih <kofanskih08@gmail.com>
2025-08-02 18:29:12 +0300

    Added BaseTest

---
+++ ui-selenium/src/test/java/com/automationexercise/ui/tests/BaseTest.java
@@ -0,0 +1,41 @@
+package com.automationexercise.ui.tests;
+
+import com.automationexercise.utils.ReadConfigs;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        String browser = ReadConfigs.getBrowserCh();
+
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
+
+        driver.manage().window().maximize();
+        driver.get(ReadConfigs.getMainUrl());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
