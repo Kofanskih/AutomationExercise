1d1c3d79db3d7e1a40812545433437e303b4c5b7
Kofanskih <kofanskih08@gmail.com>
2025-08-02 18:26:04 +0300

    Updated ReadUrl, added new key

--- ui-selenium/src/main/java/com/automationexercise/utils/ReadConfiguration.java
+++ ui-selenium/src/main/java/com/automationexercise/utils/ReadConfiguration.java
@@ -2,8 +2,12 @@ package com.automationexercise.utils;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources({"classpath:url.properties"})
+@Config.Sources({"classpath:config.properties"})
 public interface ReadConfiguration extends Config {
     @Key("main.url")
     String mainUrl();
+
+    @Key("browser.ch")
+    String browserCh();
+
 }
