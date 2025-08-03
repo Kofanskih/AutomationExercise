94fe4bc590865108fe899db3301c0e4df54bf099
Anastasiia Bukovtseva <44721588+Kofanskih@users.noreply.github.com>
2025-08-03 14:13:12 +0300

    Main page
    * Updated BaseTest, added options to browsers setUp
    
    * Added MainPage
    
    * Added MainPageTests

--- ui-selenium/src/test/java/com/automationexercise/ui/tests/BaseTest.java
+++ ui-selenium/src/test/java/com/automationexercise/ui/tests/BaseTest.java
@@ -4,7 +4,9 @@ import com.automationexercise.utils.ReadConfigs;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -18,11 +20,19 @@ public class BaseTest {
         switch (browser.toLowerCase()) {
             case "chrome":
                 WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless=new");
+                options.addArguments("--disable-gpu");
+                options.addArguments("--window-size=1920,1080");
+                driver = new ChromeDriver(options);
                 break;
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--headless");
+                firefoxOptions.addArguments("--width=1920");
+                firefoxOptions.addArguments("--height=1080");
+                driver = new FirefoxDriver(firefoxOptions);
                 break;
             default:
                 throw new IllegalArgumentException("Unsupported browser: " + browser);
