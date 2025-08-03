b9dce377980d986038840caad24259d89cca60cc
Kofanskih <kofanskih08@gmail.com>
2025-08-03 22:37:56 +0300

    Updated LoginPage, added getLoginErrorText(), fillLoginFormWithEmptyPasswordField(), checkShowValidationMessageWhenLoginPasswordIsEmpty(), checkErrorMessage()  methods

--- ui-selenium/src/main/java/com/automationexercise/pages/LoginPage.java
+++ ui-selenium/src/main/java/com/automationexercise/pages/LoginPage.java
@@ -12,6 +12,7 @@ public class LoginPage extends BasePage{
     private final By LOGIN_EMAIL_ADDRESS_FIELD = By.cssSelector("[data-qa=\"login-email\"]");
     private final By PASSWORD_FIELD = By.cssSelector("[data-qa=\"login-password\"]");
     private final By LOGIN_BUTTON = By.cssSelector("[data-qa=\"login-button\"]");
+    private final By LOGIN_ERROR_TEXT = By.cssSelector("[style='color: red;']");
 
     public LoginPage(WebDriver driver) {
         super(driver);
@@ -27,6 +28,10 @@ public class LoginPage extends BasePage{
 
     private WebElement loginButton() {
         return driver.findElement(LOGIN_BUTTON);
+    }
+
+    public String getLoginErrorText() {
+        return driver.findElement(LOGIN_ERROR_TEXT).getText();
     }
 
     public MainPage fillLoginFormWithExistsUser(LoginPageModel loginPageModel){
@@ -52,5 +57,27 @@ public class LoginPage extends BasePage{
         String actualMessage = (String) jsExecutor.executeScript(
                 "return arguments[0].validationMessage;", emailField);
         assertEquals(expectedMessage, actualMessage);
+    }
+
+    public LoginPage fillLoginFormWithEmptyPasswordField(LoginPageModel loginPageModel){
+        emailInput().sendKeys(loginPageModel.getUserEmailAddress());
+        passwordInput().clear();
+        loginButton().click();
+
+        return this;
+    }
+
+    public void checkShowValidationMessageWhenLoginPasswordIsEmpty(String expectedMessage) {
+        WebElement passwordField = passwordInput();
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+
+        String actualMessage = (String) jsExecutor.executeScript(
+                "return arguments[0].validationMessage;", passwordField);
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    public void checkErrorMessage(String expected){
+        String actualText = getLoginErrorText();
+        assertEquals(expected, actualText);
     }
 }
