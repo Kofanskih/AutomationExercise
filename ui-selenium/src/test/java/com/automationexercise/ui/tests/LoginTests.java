6de9cc7aeb8db0cae4da4f654303156ae7ed0d7f
Kofanskih <kofanskih08@gmail.com>
2025-08-03 22:03:40 +0300

    Updated LoginTests, added userLoginWithEmptyEmailField() method

--- ui-selenium/src/test/java/com/automationexercise/ui/tests/LoginTests.java
+++ ui-selenium/src/test/java/com/automationexercise/ui/tests/LoginTests.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 
 public class LoginTests extends BaseTest{
     private final String USER_LOGGED_IN_TEXT = "Logged in as";
+    private final String EMPTY_INFO_MESSAGE = "Заполните это поле.";
 
     @Test
     public void userLoggedIn(){
@@ -16,6 +17,16 @@ public class LoginTests extends BaseTest{
                 .clickLoginLogoutButton()
                 .fillLoginFormWithExistsUser(new LoginPageModel().existUserLogin())
                 .checkUserLoggedIn(USER_LOGGED_IN_TEXT);
+    }
+
+    @Test
+    public void userLoginWithEmptyEmailField(){
+        new MainPage(driver)
+                .acceptCookies();
+        new HeaderPage(driver)
+                .clickLoginLogoutButton()
+                .fillLoginFormWithEmptyEmailField(new LoginPageModel().existUserLogin())
+                .checkShowValidationMessageWhenLoginEmailIsEmpty(EMPTY_INFO_MESSAGE);
     }
 
 }
