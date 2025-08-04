7f6d3984569edfd67b713dff7fc2e8776e853700
Kofanskih <kofanskih08@gmail.com>
2025-08-04 16:02:06 +0300

    Updated MainPageTests, added clickLogoOnTheMainPage() test

--- ui-selenium/src/test/java/com/automationexercise/ui/tests/MainPageTests.java
+++ ui-selenium/src/test/java/com/automationexercise/ui/tests/MainPageTests.java
@@ -1,16 +1,26 @@
 package com.automationexercise.ui.tests;
 
+import com.automationexercise.pages.HeaderPage;
 import com.automationexercise.pages.MainPage;
 import org.testng.annotations.Test;
 
 public class MainPageTests extends BaseTest{
     private String addedMessageOnTheModalWindow = "Your product has been added to cart.";
+    private String expectedMainURL = "https://automationexercise.com/";
 
+    @Test
+    void clickLogoOnTheMainPage(){
+        new MainPage(driver)
+                .acceptCookies();
+        new HeaderPage(driver)
+                .clickLogo()
+                .checkUrlOnTheMainPage(expectedMainURL);
+    }
     @Test
     public void addItemToTheCartOnTheMainPage() {
         new MainPage(driver)
                 .acceptCookies()
                 .addItemToTheCartOnTheMainPage()
-                .checkProductAddedMessage(addedMessageOnTheModalWindow);;
+                .checkProductAddedMessage(addedMessageOnTheModalWindow);
     }
 }
