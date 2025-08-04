2835c10bd9ea8cb7f5d97c7ae2fc1f799a277370
Kofanskih <kofanskih08@gmail.com>
2025-08-04 15:34:36 +0300

    Updated BasePage, added waitUntilClickable(), waitUntilVisible() methods

--- ui-selenium/src/main/java/com/automationexercise/pages/BasePage.java
+++ ui-selenium/src/main/java/com/automationexercise/pages/BasePage.java
@@ -1,6 +1,9 @@
 package com.automationexercise.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -12,6 +15,14 @@ public class BasePage {
     public BasePage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    protected WebElement waitUntilClickable(By locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    protected WebElement waitUntilVisible(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
 }
