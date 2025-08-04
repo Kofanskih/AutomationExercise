41cb5732c66ced9089626d1ad3ce5b2e5b0ef598
Kofanskih <kofanskih08@gmail.com>
2025-08-04 15:43:05 +0300

    Updated MainPage, several methods have been changed for better readability and simplification, added methods to check adding products to the cart

--- ui-selenium/src/main/java/com/automationexercise/pages/MainPage.java
+++ ui-selenium/src/main/java/com/automationexercise/pages/MainPage.java
@@ -3,39 +3,45 @@ package com.automationexercise.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class MainPage extends BasePage{
     private final By ACCEPT_COOKIES_BUTTON = By.className("fc-button-label");
     private final By LOGGED_IN_TEXT = By.xpath("//a[contains(text(), 'Logged in as')]");
+    private final By ADD_ITEM_TO_CART = By.cssSelector("[data-product-id=\"3\"]");
+    private final By ADDED_MODAL_WINDOW_TEXT = By.xpath("//div[@class='modal-body']//p[@class='text-center']");
 
     public MainPage(WebDriver driver) {
         super(driver);
     }
 
-    private WebElement acceptCookiesButton() {
-        return driver.findElement(ACCEPT_COOKIES_BUTTON);
-    }
-
     public MainPage acceptCookies() {
-        acceptCookiesButton().click();
+        waitUntilClickable(ACCEPT_COOKIES_BUTTON).click();
         return this;
     }
 
     public String isLoggedInText() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(LOGGED_IN_TEXT));
-        return driver.findElement(LOGGED_IN_TEXT).getText();
+        return waitUntilVisible(LOGGED_IN_TEXT).getText();
     }
 
-    public void checkUserLoggedIn(String expectedSubstring){
-        String actualText = isLoggedInText().trim();
-        assertTrue(actualText.contains(expectedSubstring),
-                "The text was expected to contain: " + expectedSubstring + ", but was: " + actualText);
+    public void checkUserLoggedInText(String expectedSubstring){
+        String actualText = isLoggedInText();
+        assertTrue(actualText.contains(expectedSubstring));
+    }
+
+    public MainPage addItemToTheCartOnTheMainPage() {
+        waitUntilClickable(ADD_ITEM_TO_CART).click();
+        return this;
+    }
+
+    private WebElement addedModalWindow() {
+        return waitUntilVisible(ADDED_MODAL_WINDOW_TEXT);
+    }
+
+    public void checkProductAddedMessage(String infoModalMessage) {
+        String actualText = addedModalWindow().getText();
+        assertEquals(actualText, infoModalMessage);
     }
 }
