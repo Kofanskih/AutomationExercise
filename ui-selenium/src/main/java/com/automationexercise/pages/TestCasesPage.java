5f6533e0a86a4981d887b55678db73941d7cd1f1
Kofanskih <kofanskih08@gmail.com>
2025-08-05 14:48:16 +0300

    Updated TestCasesPage, added collapseExpandedList(), checkTheListIsCollapsed() tests

--- ui-selenium/src/main/java/com/automationexercise/pages/TestCasesPage.java
+++ ui-selenium/src/main/java/com/automationexercise/pages/TestCasesPage.java
@@ -9,6 +9,7 @@ public class TestCasesPage extends BasePage{
     private final By CENTER_TEXT_TITLE = By.cssSelector("[class=\"title text-center\"]");
     private final By LIST = By.cssSelector("[href=\"#collapse1\"]");
     private final By EXPANDED_BLOCK = By.xpath("//div[@id=\"collapse1\"]/ul[@class=\"list-group\"]");
+    private final By COLLAPSED_BLOCK = By.xpath("//div[@id=\"collapse1\"][@class=\"panel-collapse collapsing\"]");
 
     public TestCasesPage(WebDriver driver) {
         super(driver);
@@ -26,5 +27,15 @@ public class TestCasesPage extends BasePage{
 
     public void checkTheListIsExpanded(){
         waitUntilVisible(EXPANDED_BLOCK).isDisplayed();
+    }
+
+    public TestCasesPage collapseExpandedList(){
+        waitUntilVisible(EXPANDED_BLOCK).isDisplayed();
+        waitUntilClickable(LIST).click();
+        return this;
+    }
+
+    public void checkTheListIsCollapsed(){
+        waitUntilVisible(COLLAPSED_BLOCK).isDisplayed();
     }
 }
