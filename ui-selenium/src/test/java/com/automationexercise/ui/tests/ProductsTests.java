0bdd7522edce8e3f807618dc4116aa0cd6da9018
Kofanskih <kofanskih08@gmail.com>
2025-08-05 15:11:42 +0300

    Added ProductsTests, added searchItemOnTheProductsPage() test

---
+++ ui-selenium/src/test/java/com/automationexercise/ui/tests/ProductsTests.java
@@ -0,0 +1,17 @@
+package com.automationexercise.ui.tests;
+
+import com.automationexercise.pages.HeaderPage;
+import com.automationexercise.pages.MainPage;
+import org.testng.annotations.Test;
+
+public class ProductsTests extends BaseTest{
+    private String searchedItem = "Winter Top";
+
+    @Test
+    void searchItemOnTheProductsPage(){
+        new MainPage(driver).acceptCookies();
+        new HeaderPage(driver).clickProductsButton()
+                .searchItemOnTheProductsPage(searchedItem)
+                .checkSearchedItemOnTheProductsPage(searchedItem);
+    }
+}
