3c401264bbbc45d2926df559c9e75ef68333436f
Kofanskih <kofanskih08@gmail.com>
2025-08-05 15:10:10 +0300

    Updated ProductsPage, added searchItemOnTheProductsPage(), checkSearchedItemOnTheProductsPage() tests

--- ui-selenium/src/main/java/com/automationexercise/pages/ProductsPage.java
+++ ui-selenium/src/main/java/com/automationexercise/pages/ProductsPage.java
@@ -7,6 +7,9 @@ import static org.testng.Assert.assertEquals;
 
 public class ProductsPage extends BasePage{
     private final By CENTER_TEXT_TITLE = By.cssSelector("[class=\"title text-center\"]");
+    private final By SEARCH_FIELD = By.id("search_product");
+    private final By SEARCH_BUTTON = By.id("submit_search");
+    private final By SEARCH_RESULT = By.xpath("//p[contains(., 'Winter Top')]");
 
     public ProductsPage(WebDriver driver) {
         super(driver);
@@ -15,6 +18,17 @@ public class ProductsPage extends BasePage{
     public void checkTitleOnTheProductPage(String infoModalMessage) {
         String actualText = waitUntilVisible(CENTER_TEXT_TITLE).getText();
         assertEquals(actualText, infoModalMessage);
+    }
+
+    public ProductsPage searchItemOnTheProductsPage(String searchedItem){
+        waitUntilClickable(SEARCH_FIELD).sendKeys(searchedItem);
+        waitUntilClickable(SEARCH_BUTTON).click();
+        return this;
+    }
+
+    public void checkSearchedItemOnTheProductsPage(String searchedItem){
+        String actualText = waitUntilVisible(SEARCH_RESULT).getText();
+        assertEquals(actualText, searchedItem);
     }
 
 }
