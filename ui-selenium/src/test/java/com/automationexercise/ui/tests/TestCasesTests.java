46e88f20b1f1a63e069b0b04a550a5cc27ea44c4
Kofanskih <kofanskih08@gmail.com>
2025-08-05 14:29:15 +0300

    Added TestCasesTests, added goToTheTestCasesPage() test

---
+++ ui-selenium/src/test/java/com/automationexercise/ui/tests/TestCasesTests.java
@@ -0,0 +1,19 @@
+package com.automationexercise.ui.tests;
+
+import com.automationexercise.pages.HeaderPage;
+import com.automationexercise.pages.MainPage;
+import org.testng.annotations.Test;
+
+public class TestCasesTests extends BaseTest{
+    private String testCasesTitle = "TEST CASES";
+
+    @Test
+    void goToTheTestCasesPage(){
+        new MainPage(driver)
+                .acceptCookies();
+        new HeaderPage(driver)
+                .clickTestCasesButton()
+                .checkTitleOnTheTestCasesPage(testCasesTitle);
+    }
+
+}
