114c313dbb7003d70425f317553e7020476bc64f
Kofanskih <kofanskih08@gmail.com>
2025-08-05 16:43:04 +0300

    Updated ProductsTests, added closeAddedModalWindowOnTheProductsPage(), viewProductDetailsOnTheProductsPage(), goToTopsShirtsKidsCategoryOnTheProductsPage() tests

--- ui-selenium/src/test/java/com/automationexercise/ui/tests/ProductsTests.java
+++ ui-selenium/src/test/java/com/automationexercise/ui/tests/ProductsTests.java
@@ -7,21 +7,57 @@ import org.testng.annotations.Test;
 public class ProductsTests extends BaseTest{
     private String searchedItem = "Winter Top";
     private String addedMessageOnTheModalWindow = "Your product has been added to cart.";
+    private String expectedProductURL = "https://automationexercise.com/product_details/2";
+    private String kidsCategoryTitle = "KIDS - TOPS & SHIRTS PRODUCTS";
 
     @Test
     void searchItemOnTheProductsPage(){
-        new MainPage(driver).acceptCookies();
-        new HeaderPage(driver).clickProductsButton()
+        new MainPage(driver)
+                .acceptCookies();
+        new HeaderPage(driver)
+                .clickProductsButton()
                 .searchItemOnTheProductsPage(searchedItem)
                 .checkSearchedItemOnTheProductsPage(searchedItem);
     }
 
     @Test
     void addItemToTheCartOnTheProductsPage(){
-        new MainPage(driver).acceptCookies();
-        new HeaderPage(driver).clickProductsButton()
+        new MainPage(driver)
+                .acceptCookies();
+        new HeaderPage(driver)
+                .clickProductsButton()
                 .addItemToTheCartOnTheProductsPage()
                 .checkAddedItemOnTheProductsPage(addedMessageOnTheModalWindow);
+    }
 
+    @Test
+    void closeAddedModalWindowOnTheProductsPage(){
+        new MainPage(driver)
+                .acceptCookies();
+        new HeaderPage(driver)
+                .clickProductsButton()
+                .addItemToTheCartOnTheProductsPage()
+                .clickContinueShoppingButtonOnTheModalWindowOnTheProductsPage()
+                .checkModalWindowIsClosedOnTheProductsPage();
+    }
+
+    @Test
+    void viewProductDetailsOnTheProductsPage(){
+        new MainPage(driver)
+                .acceptCookies();
+        new HeaderPage(driver)
+                .clickProductsButton()
+                .clickViewProductOnTheProductsPage()
+                .checkUrlOnTheProductPage(expectedProductURL);
+    }
+
+    @Test
+    void goToTopsShirtsKidsCategoryOnTheProductsPage(){
+        new MainPage(driver)
+                .acceptCookies();
+        new HeaderPage(driver)
+                .clickProductsButton()
+                .clickOnTheKidsTopsCategoryOnTheProductsPage()
+                .checkTitleOnTheProductPage(kidsCategoryTitle);
     }
 }
