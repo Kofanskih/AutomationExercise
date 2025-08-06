fc13a508f63515d2c74eee4f1ea5e767b1951a40
Kofanskih <kofanskih08@gmail.com>
2025-08-06 23:38:53 +0300

    Added PaymentTests, added payAndConfirmOrderWithCorrectData() test

---
+++ ui-selenium/src/test/java/com/automationexercise/ui/tests/PaymentTests.java
@@ -0,0 +1,28 @@
+package com.automationexercise.ui.tests;
+
+import com.automationexercise.pageModels.LoginPageModel;
+import com.automationexercise.pageModels.PaymentPageModel;
+import com.automationexercise.pages.HeaderPage;
+import com.automationexercise.pages.MainPage;
+import org.testng.annotations.Test;
+
+public class PaymentTests extends BaseTest{
+    private String title = "ORDER PLACED!";
+
+    @Test
+    void payAndConfirmOrderWithCorrectData() {
+        new MainPage(driver)
+                .acceptCookies();
+        new HeaderPage(driver)
+                .clickLoginLogoutButton()
+                .fillLoginForm(new LoginPageModel().existUserLogin());
+        new HeaderPage(driver)
+                .clickProductsButton()
+                .addItemToTheCartOnTheProductsPage()
+                .clickOnTheViewCartButtonOnTheModalWindow()
+                .clickToCheckoutButtonOnTheCartPage()
+                .clickPlaceOrderButton()
+                .fillPaymentForm(new PaymentPageModel().existPaymentData())
+                .checkPlaceOrderTitle(title);
+    }
+}
