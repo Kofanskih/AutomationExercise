c41894d3a5f96d97b767266b5861b721e4da5020
Kofanskih <kofanskih08@gmail.com>
2025-08-06 23:38:05 +0300

    Updated PaymentPage, added fillPaymentForm(), checkPlaceOrderTitle() methods

--- ui-selenium/src/main/java/com/automationexercise/pages/PaymentPage.java
+++ ui-selenium/src/main/java/com/automationexercise/pages/PaymentPage.java
@@ -1,10 +1,20 @@
 package com.automationexercise.pages;
 
+import com.automationexercise.pageModels.PaymentPageModel;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import static org.testng.Assert.assertEquals;
 
 public class PaymentPage extends BasePage{
+    private final By NAME_ON_CARD_FIELD = By.cssSelector("[name=\"name_on_card\"]");
+    private final By CARD_NUMBER_FIELD = By.cssSelector("[data-qa=\"card-number\"]");
+    private final By CVC_FIELD = By.cssSelector("[name=\"cvc\"]");
+    private final By EXPIRY_MONTH_FIELD = By.cssSelector("[name=\"expiry_month\"]");
+    private final By EXPIRY_YEAR_FIELD = By.cssSelector("[data-qa=\"expiry-year\"]");
+    private final By PAY_AND_CONFIRM_BUTTON = By.cssSelector("[data-qa=\"pay-button\"]");
+    private final By TITLE = By.cssSelector("[data-qa=\"order-placed\"]");
 
     public PaymentPage(WebDriver driver) {
         super(driver);
@@ -13,5 +23,49 @@ public class PaymentPage extends BasePage{
     public void checkUrlOnThePaymentPage(String expectedUrl){
         String actualUrl = driver.getCurrentUrl();
         assertEquals(actualUrl, expectedUrl);
+    }
+
+    private WebElement nameInput() {
+        return driver.findElement(NAME_ON_CARD_FIELD);
+    }
+
+    private WebElement cardNumberInput() {
+        return driver.findElement(CARD_NUMBER_FIELD);
+    }
+
+    private WebElement cvcInput() {
+        return driver.findElement(CVC_FIELD);
+    }
+
+    private WebElement monthInput() {
+        return driver.findElement(EXPIRY_MONTH_FIELD);
+    }
+
+    private WebElement yearInput() {
+        return driver.findElement(EXPIRY_YEAR_FIELD);
+    }
+
+    private WebElement payConfirmButton() {
+        return driver.findElement(PAY_AND_CONFIRM_BUTTON);
+    }
+
+    private WebElement placeOrderTitle() {
+        return driver.findElement(TITLE);
+    }
+
+    public PaymentPage fillPaymentForm(PaymentPageModel paymentPageModel){
+        nameInput().sendKeys(paymentPageModel.getNameOnCard());
+        cardNumberInput().sendKeys(paymentPageModel.getCardNumber());
+        cvcInput().sendKeys(paymentPageModel.getCvc());
+        monthInput().sendKeys(paymentPageModel.getMonth());
+        yearInput().sendKeys(paymentPageModel.getYear());
+        payConfirmButton().click();
+
+        return this;
+    }
+
+    public void checkPlaceOrderTitle(String title) {
+        String actualText = placeOrderTitle().getText();
+        assertEquals(actualText, title);
     }
 }
