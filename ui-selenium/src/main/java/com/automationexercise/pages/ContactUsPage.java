0465cf270ae8b54098439bdee1a5368eb472132b
Kofanskih <kofanskih08@gmail.com>
2025-08-08 15:21:50 +0300

    Added ContactUsPage, added fillMessageForm(), clickOKButton(), checkMessageWasSent() methods

---
+++ ui-selenium/src/main/java/com/automationexercise/pages/ContactUsPage.java
@@ -0,0 +1,60 @@
+package com.automationexercise.pages;
+
+import com.automationexercise.pageModels.ContactUsPageModel;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static org.testng.Assert.assertEquals;
+
+public class ContactUsPage extends BasePage{
+    private final By NAME_FIELD = By.cssSelector("[data-qa=\"name\"]");
+    private final By EMAIL_FIELD = By.cssSelector("[data-qa=\"email\"]");
+    private final By SUBJECT_FIELD = By.cssSelector("[data-qa=\"subject\"]");
+    private final By MESSAGE_FIELD = By.cssSelector("[data-qa=\"message\"]");
+    private final By SUBMIT_BUTTON = By.cssSelector("[data-qa=\"submit-button\"]");
+    private final By ALERT_SUCCESS = By.cssSelector("[class=\"status alert alert-success\"]");
+
+    public ContactUsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    private WebElement nameInput() {
+        return driver.findElement(NAME_FIELD);
+    }
+
+    private WebElement emailInput() {
+        return driver.findElement(EMAIL_FIELD);
+    }
+
+    private WebElement subjectInput() {
+        return driver.findElement(SUBJECT_FIELD);
+    }
+
+    private WebElement messageInput() {
+        return driver.findElement(MESSAGE_FIELD);
+    }
+
+    private WebElement submitButton() {
+        return driver.findElement(SUBMIT_BUTTON);
+    }
+
+    public ContactUsPage fillMessageForm(ContactUsPageModel contactUsPageModel){
+        nameInput().sendKeys(contactUsPageModel.getUserName());
+        emailInput().sendKeys(contactUsPageModel.getUserEmailAddress());
+        subjectInput().sendKeys(contactUsPageModel.getUserSubject());
+        messageInput().sendKeys(contactUsPageModel.getUserMessage());
+        submitButton().click();
+        return this;
+    }
+
+    public ContactUsPage clickOKButton() {
+        driver.switchTo().alert().accept();
+        return this;
+    }
+
+    public void checkMessageWasSent(String title) {
+        String actualText = waitUntilVisible(ALERT_SUCCESS).getText();
+        assertEquals(actualText, title);
+    }
+}
