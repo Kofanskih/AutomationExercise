cbef989c8601d13f7bfbb68715141cd1e6cf17bc
Kofanskih <kofanskih08@gmail.com>
2025-08-08 15:23:41 +0300

    Added ContactUsTests, added sendContactMessage() test

---
+++ ui-selenium/src/test/java/com/automationexercise/ui/tests/ContactUsTests.java
@@ -0,0 +1,19 @@
+package com.automationexercise.ui.tests;
+
+import com.automationexercise.pageModels.ContactUsPageModel;
+import com.automationexercise.pages.HeaderPage;
+import com.automationexercise.pages.MainPage;
+import org.testng.annotations.Test;
+
+public class ContactUsTests extends BaseTest{
+    private String successfulMessage = "Success! Your details have been submitted successfully.";
+
+    @Test
+    void sendContactMessage(){
+        new MainPage(driver).acceptCookies();
+        new HeaderPage(driver).clickContactUsButton()
+                .fillMessageForm(new ContactUsPageModel().messageForm())
+                .clickOKButton()
+                .checkMessageWasSent(successfulMessage);
+    }
+}
