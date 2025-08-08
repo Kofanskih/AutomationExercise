bcd134109c7584051663561e2096b57b349185e3
Kofanskih <kofanskih08@gmail.com>
2025-08-08 16:00:42 +0300

    Updated ContactUsTests, added fillContactUsFormWithEmptyEmailField() test

--- ui-selenium/src/test/java/com/automationexercise/ui/tests/ContactUsTests.java
+++ ui-selenium/src/test/java/com/automationexercise/ui/tests/ContactUsTests.java
@@ -8,8 +8,8 @@ import org.testng.annotations.Test;
 public class ContactUsTests extends BaseTest{
     private String successfulMessage = "Success! Your details have been submitted successfully.";
     private String expectedMainURL = "https://automationexercise.com/";
-    private String invalidEmailValidationMessage = "1Адрес электронной почты должен содержать символ \"@\". В адресе \"brbrbbrbr.com\" отсутствует символ \"@\".";
-
+    private String invalidEmailValidationMessage = "Адрес электронной почты должен содержать символ \"@\". В адресе \"brbrbbrbr.com\" отсутствует символ \"@\".";
+    private String emptyEmailValidationMessage = "Заполните это поле.";
 
     @Test
     void sendContactMessage(){
@@ -46,12 +46,22 @@ public class ContactUsTests extends BaseTest{
     }
 
     @Test
-    void fillContactUsformWithInvalidEmail(){
+    void fillContactUsFormWithInvalidEmail(){
         new MainPage(driver)
                 .acceptCookies();
         new HeaderPage(driver)
                 .clickContactUsButton()
                 .fillMessageForm(new ContactUsPageModel().messageFormInvalidEmail())
                 .checkShowValidationMessageInEmailField(invalidEmailValidationMessage);
+    }
+
+    @Test
+    void fillContactUsFormWithEmptyEmailField(){
+        new MainPage(driver)
+                .acceptCookies();
+        new HeaderPage(driver)
+                .clickContactUsButton()
+                .fillMessageForm(new ContactUsPageModel().messageFormEmptyEmail())
+                .checkShowValidationMessageInEmailField(emptyEmailValidationMessage);
     }
 }
