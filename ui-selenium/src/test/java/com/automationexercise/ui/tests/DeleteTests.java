b1bba3da40d9e16b06e3c43f8adc83ed622321bf
Kofanskih <kofanskih08@gmail.com>
2025-08-09 17:36:35 +0300

    Updated DeleteTests, userTryToLoginAfterDeletingAccount() test

--- ui-selenium/src/test/java/com/automationexercise/ui/tests/DeleteTests.java
+++ ui-selenium/src/test/java/com/automationexercise/ui/tests/DeleteTests.java
@@ -1,13 +1,16 @@
 package com.automationexercise.ui.tests;
 
+import com.automationexercise.pageModels.LoginPageModel;
 import com.automationexercise.pageModels.RegistrationPageModel;
 import com.automationexercise.pages.HeaderPage;
+import com.automationexercise.pages.LoginPage;
 import com.automationexercise.pages.MainPage;
 import org.testng.annotations.Test;
 
 public class DeleteTests extends BaseTest{
     private String expectedMainURL = "https://automationexercise.com/";
     private String title = "ACCOUNT DELETED!";
+    private String loginErrorMessage = "Your email or password is incorrect!";
 
     @Test
     public void deleteUser(){
@@ -48,5 +51,30 @@ public class DeleteTests extends BaseTest{
         new HeaderPage(driver)
                 .clickDeleteAccountButton()
                 .checkTitleAfterDeletingAccount(title);
+    }
+
+    @Test
+    void userTryToLoginAfterDeletingAccount(){
+        new MainPage(driver)
+                .acceptCookies();
+        new HeaderPage(driver)
+                .clickLoginLogoutButton()
+                .fillSignUpForm(new RegistrationPageModel().getRegistrationUserDataForDeleting())
+                .chooseGenderFemale()
+                .fillUserData(new RegistrationPageModel().getRegistrationUserDataForDeleting())
+                .chooseCountry()
+                .fillDateOfBirthDropdowns()
+                .markNewsletterCheckbox()
+                .markSpecialOfferCheckbox()
+                .clickCreateAccountButton()
+                .clickContinueButton();
+        new HeaderPage(driver)
+                .clickDeleteAccountButton()
+                .clickContinueButtonOnTheDeletePage();
+        new HeaderPage(driver)
+                .clickLoginLogoutButton()
+                .fillLoginForm(new LoginPageModel().loginForDelete());
+        new LoginPage(driver)
+                .checkLoginErrorMessage(loginErrorMessage);
     }
 }
