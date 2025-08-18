e93fb8d02eb4ec5af50ec414c73ac5ba895b63b6
Kofanskih <kofanskih08@gmail.com>
2025-08-18 22:49:40 +0300

    Updated FooterTests, added test description and allure annotations

--- ui-selenium/src/test/java/com/automationexercise/ui/tests/FooterTests.java
+++ ui-selenium/src/test/java/com/automationexercise/ui/tests/FooterTests.java
@@ -3,13 +3,20 @@ package com.automationexercise.ui.tests;
 import com.automationexercise.pageModels.RegistrationPageModel;
 import com.automationexercise.pages.FooterPage;
 import com.automationexercise.pages.MainPage;
+import io.qameta.allure.*;
 import org.testng.annotations.Test;
 
+@Epic("Subscription")
+@Feature("User subscription")
+@Story("Subscription")
+@Severity(SeverityLevel.NORMAL)
+@Owner("bukovtseva")
+@TmsLink("TC-007")
 public class FooterTests extends BaseTest{
     private String successfulSubscriptionValidationMessage = "You have been successfully subscribed!";
     private String errorValidationMessage = "Адрес электронной почты должен содержать символ \"@\". В адресе \"test.com\" отсутствует символ \"@\".";
 
-    @Test
+    @Test(description = "User subscribed")
     void userSubscribe(){
         new MainPage(driver)
                 .acceptCookies();
@@ -17,7 +24,7 @@ public class FooterTests extends BaseTest{
                 .checkUserIsSubscribed(new RegistrationPageModel().getSubscribeEmail(), successfulSubscriptionValidationMessage);
     }
 
-    @Test
+    @Test(description = "Try to subscribe with invalid email")
     void userSubscribeWithInvalidEmail(){
         new MainPage(driver)
                 .acceptCookies();
