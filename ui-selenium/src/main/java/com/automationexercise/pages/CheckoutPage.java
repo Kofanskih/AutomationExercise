865aebf5fffbaf5ad983bd19ab1109e5a29afbc0
Kofanskih <kofanskih08@gmail.com>
2025-08-18 15:25:43 +0300

    Updated CheckoutPage, added allure steps

--- ui-selenium/src/main/java/com/automationexercise/pages/CheckoutPage.java
+++ ui-selenium/src/main/java/com/automationexercise/pages/CheckoutPage.java
@@ -1,5 +1,6 @@
 package com.automationexercise.pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
@@ -12,16 +13,19 @@ public class CheckoutPage extends BasePage{
         super(driver);
     }
 
+    @Step("Check URL on the Checkout page")
     public void checkUrlOnTheCheckoutPage(String expectedUrl) {
         String actualUrl = driver.getCurrentUrl();
         Assert.assertEquals(actualUrl, expectedUrl);
     }
 
+    @Step("Go to The Login page")
     public LoginPage goToTheLoginPage(){
         waitUntilClickable(REGISTER_LOGIN_BUTTON).click();
         return new LoginPage(driver);
     }
 
+    @Step("Click Place order button")
     public PaymentPage clickPlaceOrderButton(){
         waitUntilClickable(PLACE_ORDER_BUTTON).click();
         return new PaymentPage(driver);
