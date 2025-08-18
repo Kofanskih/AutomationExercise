ce0c6aa21c1d670a694d6d0a7b0347dc849df456
Kofanskih <kofanskih08@gmail.com>
2025-08-18 15:02:44 +0300

    Updated LoginPageModel, added allure steps

--- ui-selenium/src/main/java/com/automationexercise/pageModels/LoginPageModel.java
+++ ui-selenium/src/main/java/com/automationexercise/pageModels/LoginPageModel.java
@@ -1,5 +1,6 @@
 package com.automationexercise.pageModels;
 
+import io.qameta.allure.Step;
 import lombok.Data;
 
 @Data
@@ -7,24 +8,28 @@ public class LoginPageModel {
     private String userEmailAddress;
     private String userPassword;
 
+    @Step("Use correct login")
     public LoginPageModel existUserLogin(){
         this.userEmailAddress = "1test1test@test.com";
         this.userPassword = "12345678";
         return this;
     }
 
+    @Step("Use incorrect login")
     public LoginPageModel incorrectDataLogin(){
         this.userEmailAddress = "1test1test@test.com";
         this.userPassword = "1234567";
         return this;
     }
 
+    @Step("Use invalid email login")
     public LoginPageModel invalidEmailLogin(){
         this.userEmailAddress = "1test1testtest.com";
         this.userPassword = "12345678";
         return this;
     }
 
+    @Step("Use login for deleting")
     public LoginPageModel loginForDelete(){
         this.userEmailAddress = "deletingTest@gmail.com";
         this.userPassword = "12345678";
