43bf6efae64f15132eaa73707f14766926d815cc
Kofanskih <kofanskih08@gmail.com>
2025-08-18 22:54:52 +0300

    Updated LoginTests, added test description and allure annotations

--- ui-selenium/src/test/java/com/automationexercise/ui/tests/LoginTests.java
+++ ui-selenium/src/test/java/com/automationexercise/ui/tests/LoginTests.java
@@ -4,8 +4,15 @@ import com.automationexercise.pageModels.LoginPageModel;
 import com.automationexercise.pages.HeaderPage;
 import com.automationexercise.pages.LoginPage;
 import com.automationexercise.pages.MainPage;
+import io.qameta.allure.*;
 import org.testng.annotations.Test;
 
+@Epic("Authentication")
+@Feature("Login")
+@Story("User logs in with different credentials")
+@Severity(SeverityLevel.CRITICAL)
+@Owner("bukovtseva")
+@TmsLink("TC-001")
 public class LoginTests extends BaseTest{
     private final String USER_LOGGED_IN_TEXT = "Logged in as";
     private final String EMPTY_INFO_MESSAGE = "Заполните это поле.";
@@ -13,7 +20,7 @@ public class LoginTests extends BaseTest{
     private final String INVALID_EMAIL_INFO_MESSAGE  = "Адрес электронной почты должен содержать символ \"@\". В адресе \"1test1testtest.com\" отсутствует символ \"@\".";
 
 
-    @Test
+    @Test(description = "User can login with correct credentials")
     public void userLogin(){
         new MainPage(driver)
                 .acceptCookies();
@@ -23,7 +30,7 @@ public class LoginTests extends BaseTest{
                 .checkUserLoggedInText(USER_LOGGED_IN_TEXT);
     }
 
-    @Test
+    @Test(description = "User can't login with incorrect credentials")
     public void userLoginWithIncorrectCredentials(){
         new MainPage(driver)
                 .acceptCookies();
@@ -34,7 +41,7 @@ public class LoginTests extends BaseTest{
                 .checkLoginErrorMessage(LOGIN_ERROR_MESSAGE);
     }
 
-    @Test
+    @Test(description = "User can't login with empty email field")
     public void userLoginWithEmptyEmailField(){
         new MainPage(driver)
                 .acceptCookies();
@@ -44,7 +51,7 @@ public class LoginTests extends BaseTest{
                 .checkShowValidationMessageWhenLoginEmailIsEmpty(EMPTY_INFO_MESSAGE);
     }
 
-    @Test
+    @Test(description = "User can't login with empty password field")
     public void userLoginWithEmptyPasswordField(){
         new MainPage(driver)
                 .acceptCookies();
@@ -54,7 +61,7 @@ public class LoginTests extends BaseTest{
                 .checkShowValidationMessageWhenLoginPasswordIsEmpty(EMPTY_INFO_MESSAGE);
     }
 
-    @Test
+    @Test(description = "User can't login with invalid email")
     public void userLoginWithInvalidEmail(){
         new MainPage(driver)
                 .acceptCookies();
@@ -64,5 +71,4 @@ public class LoginTests extends BaseTest{
         new LoginPage(driver)
                 .checkShowValidationMessageWhenEmailIsInvalid(INVALID_EMAIL_INFO_MESSAGE);
     }
-
 }
