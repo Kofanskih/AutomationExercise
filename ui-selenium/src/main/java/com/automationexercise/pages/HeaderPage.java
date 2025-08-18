c02a643c864e1b38f3f5f3876c9d7d4f9280840f
Kofanskih <kofanskih08@gmail.com>
2025-08-18 15:45:59 +0300

    Updated HeaderPage, added allure steps

--- ui-selenium/src/main/java/com/automationexercise/pages/HeaderPage.java
+++ ui-selenium/src/main/java/com/automationexercise/pages/HeaderPage.java
@@ -1,5 +1,6 @@
 package com.automationexercise.pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -20,51 +21,61 @@ public class HeaderPage extends BasePage{
         super(driver);
     }
 
+    @Step("Click the logo button on the header")
     public MainPage clickLogo(){
         waitUntilClickable(LOGO).click();
         return new MainPage(driver);
     }
 
+    @Step("Click the Home button on the header")
     public MainPage clickHomeButton(){
         waitUntilClickable(HOME_BUTTON).click();
         return new MainPage(driver);
     }
 
+    @Step("Click the Products button on the header")
     public ProductsPage clickProductsButton(){
         waitUntilClickable(PRODUCTS_BUTTON).click();
         return new ProductsPage(driver);
     }
 
+    @Step("Click the Cart button on the header")
     public CartPage clickCartButton(){
         waitUntilClickable(CART_BUTTON).click();
         return new CartPage(driver);
     }
 
+    @Step("Click the login/logout button on the header")
     public LoginPage clickLoginLogoutButton(){
         waitUntilClickable(LOGIN_LOGOUT_BUTTON).click();
         return new LoginPage(driver);
     }
 
+    @Step("Click the Delete button on the header")
     public DeletePage clickDeleteAccountButton(){
         waitUntilClickable(DELETE_ACCOUNT_BUTTON).click();
         return new DeletePage(driver);
     }
 
+    @Step("Click the TestCases button on the header")
     public TestCasesPage clickTestCasesButton(){
         waitUntilClickable(TEST_CASES_BUTTON).click();
         return new TestCasesPage(driver);
     }
 
+    @Step("Click the APITesting button on the header")
     public APITestingPage clickAPITestingButton(){
         waitUntilClickable(API_TESTING_BUTTON).click();
         return new APITestingPage(driver);
     }
 
+    @Step("Click the Video Tutorials button on the header")
     public VideoTutorialsPage clickVideoTutorialsButton(){
         waitUntilClickable(VIDEO_TUTORIALS_BUTTON).click();
         return new VideoTutorialsPage(driver);
     }
 
+    @Step("Click the Contact Us button on the header")
     public ContactUsPage clickContactUsButton(){
         waitUntilClickable(CONTACT_US_BUTTON).click();
         return new ContactUsPage(driver);
