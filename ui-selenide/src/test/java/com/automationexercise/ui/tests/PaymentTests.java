b0949be0685cc681b677cd7d4117795725bfd6dd
Kofanskih <kofanskih08@gmail.com>
2025-08-19 20:39:12 +0300

    Added PaymentTests, added payAndConfirmOrderWithCorrectData test

---
+++ ui-selenide/src/test/java/com/automationexercise/ui/tests/PaymentTests.java
@@ -0,0 +1,40 @@
+package com.automationexercise.ui.tests;
+
+import com.automationexercise.pageModels.LoginPageModel;
+import com.automationexercise.pageModels.PaymentPageModel;
+import com.automationexercise.pages.BasePage;
+import com.automationexercise.pages.HeaderPage;
+import com.automationexercise.pages.MainPage;
+import com.automationexercise.utils.ConfigurateBrowserSettings;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class PaymentTests {
+    private String title = "ORDER PLACED!";
+
+    @BeforeMethod
+    void preconditionMethod(){
+        new ConfigurateBrowserSettings().setUp();
+        new BasePage().open();
+    }
+
+    @AfterMethod
+    void postconditionMethod(){
+        new BasePage().close();
+    }
+
+    @Test(description = "Pay and confirm order with correct data")
+    void payAndConfirmOrderWithCorrectData() {
+        new MainPage().acceptCookies();
+        new HeaderPage().clickLoginButton()
+                .fillLoginForm(new LoginPageModel().myLogin());
+        new HeaderPage().clickProductsButton()
+                .addItemToTheCartOnTheProductsPage()
+                .clickOnTheViewCartButtonOnTheModalWindow()
+                .clickToCheckoutButtonOnTheCartPage()
+                .clickPlaceOrderButton()
+                .fillPaymentForm(new PaymentPageModel().existPaymentData())
+                .checkPlaceOrderTitle(title);
+    }
+}
