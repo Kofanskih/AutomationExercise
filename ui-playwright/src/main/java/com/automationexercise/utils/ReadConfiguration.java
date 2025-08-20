7ed86ebad4e77243545faeba7b6430a0010051e1
Kofanskih <kofanskih08@gmail.com>
2025-08-20 23:00:23 +0300

    Added ReadConfiguration interface

---
+++ ui-playwright/src/main/java/com/automationexercise/utils/ReadConfiguration.java
@@ -0,0 +1,28 @@
+package com.automationexercise.utils;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({"system:properties", "classpath:config.properties", "classpath:allure.properties"})
+public interface ReadConfiguration extends Config {
+    @Key("main.url")
+    String mainUrl();
+
+    @Key("allure.results.directory")
+    String allureResultsDir();
+
+
+    @Key("base.test.video.path")
+    String baseTestVideoPath();
+
+    String browser();
+
+    boolean headless();
+
+    @Key("slow.motion")
+    int slowMotion();
+
+    int timeout();
+
+    boolean video();
+}
