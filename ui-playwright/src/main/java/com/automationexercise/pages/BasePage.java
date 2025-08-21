6566344aa0e4d76435706466cb88f623318b882f
Kofanskih <kofanskih08@gmail.com>
2025-08-21 22:45:40 +0300

    Updated BasePage, added open() methods

--- ui-playwright/src/main/java/com/automationexercise/pages/BasePage.java
+++ ui-playwright/src/main/java/com/automationexercise/pages/BasePage.java
@@ -1,17 +1,31 @@
 package com.automationexercise.pages;
 
 import com.microsoft.playwright.Page;
+import io.qameta.allure.Step;
 
 import static com.automationexercise.config.ConfigurationManager.config;
 
 public abstract class BasePage {
     protected Page page;
 
-    public void setAndConfigurePage(final Page page){
+    public BasePage(Page page) {
+        this.page = page;
+    }
+
+    @Step("Set browser configuration")
+    public void configurePage(final Page page){
         this.page = page;
         page.setDefaultTimeout(config().timeout());
     }
 
-    public void initComponents(){
+    @Step("Navigate to the main page")
+    public MainPage open() {
+        page.navigate(config().mainUrl());
+        return new MainPage(page);
+    }
+
+    @Step("Verify page title")
+    public String getPageTitle() {
+        return page.title();
     }
 }
