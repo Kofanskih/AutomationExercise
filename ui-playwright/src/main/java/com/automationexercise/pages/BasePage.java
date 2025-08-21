d3eb3f08b9e5e403c88ac7af32ead8d81ce2539e
Kofanskih <kofanskih08@gmail.com>
2025-08-21 14:37:46 +0300

    Added BasePage

---
+++ ui-playwright/src/main/java/com/automationexercise/pages/BasePage.java
@@ -0,0 +1,17 @@
+package com.automationexercise.pages;
+
+import com.microsoft.playwright.Page;
+
+import static com.automationexercise.config.ConfigurationManager.config;
+
+public abstract class BasePage {
+    protected Page page;
+
+    public void setAndConfigurePage(final Page page){
+        this.page = page;
+        page.setDefaultTimeout(config().timeout());
+    }
+
+    public void initComponents(){
+    }
+}
