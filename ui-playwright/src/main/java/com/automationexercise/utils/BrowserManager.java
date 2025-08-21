0662cf5c50a4c25fc859316c906dfb73563900bd
Kofanskih <kofanskih08@gmail.com>
2025-08-21 22:47:42 +0300

    Updated BrowserManager, added createContext() method

--- ui-playwright/src/main/java/com/automationexercise/utils/BrowserManager.java
+++ ui-playwright/src/main/java/com/automationexercise/utils/BrowserManager.java
@@ -1,6 +1,7 @@
 package com.automationexercise.utils;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.Playwright;
 
 import static com.automationexercise.config.ConfigurationManager.config;
@@ -8,5 +9,10 @@ import static com.automationexercise.config.ConfigurationManager.config;
 public class BrowserManager {
     public static Browser getBrowser(final Playwright playwright){
         return BrowserFactory.valueOf(config().browser().toUpperCase()).createInstance(playwright);
+    }
+
+    public static BrowserContext createContext(Browser browser) {
+        return browser.newContext(new Browser.NewContextOptions()
+                .setViewportSize(config().viewportWidth(), config().viewportHeight()));
     }
 }
