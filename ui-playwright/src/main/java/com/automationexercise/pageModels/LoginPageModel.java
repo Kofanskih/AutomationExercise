09ebd23dd49db0ebc2b46935d596c6dd7175252c
Kofanskih <kofanskih08@gmail.com>
2025-08-22 15:19:45 +0300

    Added LoginPageModel

---
+++ ui-playwright/src/main/java/com/automationexercise/pageModels/LoginPageModel.java
@@ -0,0 +1,17 @@
+package com.automationexercise.pageModels;
+
+import io.qameta.allure.Step;
+import lombok.Data;
+
+@Data
+public class LoginPageModel {
+    private String userEmailAddress;
+    private String userPassword;
+
+    @Step("Use correct login")
+    public LoginPageModel myLogin(){
+        this.userEmailAddress = "1test1test@test.com";
+        this.userPassword = "12345678";
+        return this;
+    }
+}
