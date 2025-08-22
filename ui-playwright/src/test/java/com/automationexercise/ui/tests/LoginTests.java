8b88a1b6a9154d52888aa92bdd53e9bf337a5a5c
Kofanskih <kofanskih08@gmail.com>
2025-08-22 15:50:46 +0300

    Added LoginTests, added userLoginWithIncorrectCredentials() test

--- ui-playwright/src/test/java/com/automationexercise/ui/tests/LoginTests.java
+++ ui-playwright/src/test/java/com/automationexercise/ui/tests/LoginTests.java
@@ -2,11 +2,13 @@ package com.automationexercise.ui.tests;
 
 import com.automationexercise.pageModels.LoginPageModel;
 import com.automationexercise.pages.HeaderPage;
+import com.automationexercise.pages.LoginPage;
 import com.automationexercise.pages.MainPage;
 import org.testng.annotations.Test;
 
 public class LoginTests extends BaseTest {
     private final String USER_LOGGED_IN_TEXT = "Logged in as";
+    private final String LOGIN_ERROR_MESSAGE = "Your email or password is incorrect!";
 
     @Test(description = "User can login with correct credentials")
     public void userLogin(){
@@ -16,6 +18,17 @@ public class LoginTests extends BaseTest {
                 .clickLoginButton()
                 .fillLoginForm(new LoginPageModel().myLogin())
                 .checkUserLoggedInText(USER_LOGGED_IN_TEXT);
+    }
+
+    @Test(description = "User can't login with incorrect credentials")
+    public void userLoginWithIncorrectCredentials(){
+        new MainPage(page)
+                .acceptCookies();
+        new HeaderPage(page)
+                .clickLoginButton()
+                .fillLoginForm(new LoginPageModel().incorrectDataLogin());
+        new LoginPage(page)
+                .checkLoginErrorMessage(LOGIN_ERROR_MESSAGE);
     }
 
 }
