169d55e38ee6d5474e7f7ef3427c1a98263edd25
Kofanskih <kofanskih08@gmail.com>
2025-08-22 16:21:50 +0300

    Updated LoginPage, added fillLoginFormWithEmptyEmailField(), getValidationMessage(), checkShowValidationMessageWhenLoginEmailIsEmpty() method

--- ui-playwright/src/main/java/com/automationexercise/pages/LoginPage.java
+++ ui-playwright/src/main/java/com/automationexercise/pages/LoginPage.java
@@ -8,7 +8,7 @@ import io.qameta.allure.Step;
 import static org.testng.AssertJUnit.assertEquals;
 
 public class LoginPage extends BasePage{
-    private final String LOGIN_EMAIL_ADDRESS_FIELD = "input[name='email']";
+    private final String LOGIN_EMAIL_ADDRESS_FIELD = "input[data-qa='login-email']";
     private final String PASSWORD_FIELD = "input[name='password']";
     private final String LOGIN_BUTTON = "button[data-qa='login-button']";
     private final Locator LOGIN_ERROR_TEXT = page.locator("[style=\"color: red;\"]");
@@ -31,4 +31,25 @@ public class LoginPage extends BasePage{
         String actualText = LOGIN_ERROR_TEXT.innerText();
         assertEquals(expected, actualText);
     }
+
+    @Step ("Fill login form with empty email field and click login button")
+    public LoginPage fillLoginFormWithEmptyEmailField(LoginPageModel loginPageModel){
+        page.fill(PASSWORD_FIELD, loginPageModel.getUserPassword());
+        page.click(LOGIN_BUTTON);
+
+        return this;
+    }
+    public String getValidationMessage(Locator element) {
+        return (String) element.evaluate("el => el.validationMessage");
+    }
+
+    @Step("Check validation message if login email field is empty")
+    public void checkShowValidationMessageWhenLoginEmailIsEmpty(String expectedMessage) {
+        Locator emailField = page.locator(LOGIN_EMAIL_ADDRESS_FIELD);
+        String actualMessage = getValidationMessage(emailField);
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+
 }
