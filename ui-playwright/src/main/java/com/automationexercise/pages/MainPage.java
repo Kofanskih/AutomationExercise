c990d3dcc2604c94958138a08b04f7f8bc4b6664
Kofanskih <kofanskih08@gmail.com>
2025-08-23 19:17:20 +0300

    Updated MainPage, added addItemToTheCartOnTheMainPage(), checkProductAddedMessage() methods

--- ui-playwright/src/main/java/com/automationexercise/pages/MainPage.java
+++ ui-playwright/src/main/java/com/automationexercise/pages/MainPage.java
@@ -5,11 +5,14 @@ import com.microsoft.playwright.Page;
 import io.qameta.allure.Step;
 import org.testng.Assert;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.AssertJUnit.assertTrue;
 
 public class MainPage extends BasePage{
     private final Locator ACCEPT_COOKIES_BUTTON = page.locator(".fc-button-label").filter(new Locator.FilterOptions().setHasText("Соглашаюсь"));
     private final Locator USER_LOGGED_IN_TEXT = page.locator("text=Logged in as");
+    private final Locator ADD_ITEM_TO_CART = page.locator("a[data-product-id='3']").first();
+    private final Locator ADDED_MODAL_WINDOW_TEXT = page.locator("text=Your product has been added to cart.");
 
     public MainPage(Page page) {
         super(page);
@@ -33,4 +36,15 @@ public class MainPage extends BasePage{
         Assert.assertEquals(actualUrl, expectedUrl);
     }
 
+    @Step("Add item to the cart")
+    public MainPage addItemToTheCartOnTheMainPage() {
+        ADD_ITEM_TO_CART.click();
+        return this;
+    }
+
+    @Step("Check added item on the Main page")
+    public void checkProductAddedMessage(String infoModalMessage) {
+        String actualText = ADDED_MODAL_WINDOW_TEXT.innerText();
+        assertEquals(actualText, infoModalMessage);
+    }
 }
