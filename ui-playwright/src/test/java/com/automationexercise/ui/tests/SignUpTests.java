c727aa91b928816b780645c6404396f9d8de6bb5
Kofanskih <kofanskih08@gmail.com>
2025-08-23 15:30:04 +0300

    Added SignUpTests, added userSignUp() test

---
+++ ui-playwright/src/test/java/com/automationexercise/ui/tests/SignUpTests.java
@@ -0,0 +1,35 @@
+package com.automationexercise.ui.tests;
+
+import com.automationexercise.pageModels.RegistrationPageModel;
+import com.automationexercise.pages.HeaderPage;
+import com.automationexercise.pages.MainPage;
+import io.qameta.allure.*;
+import org.testng.annotations.Test;
+
+@Epic("Authentication")
+@Feature("User registration")
+@Story("Registration")
+@Severity(SeverityLevel.CRITICAL)
+@Owner("bukovtseva")
+@TmsLink("TC-002")
+public class SignUpTests extends BaseTest{
+    private String loggedUser = "Logged in as";
+
+    @Test(description = "User registration")
+    void userSignUp(){
+        new MainPage(page)
+                .acceptCookies();
+        new HeaderPage(page)
+                .clickLoginButton()
+                .fillSignUpForm(new RegistrationPageModel().getRandomRegistrationUserData())
+                .chooseGenderFemale()
+                .fillUserData(new RegistrationPageModel().getRandomRegistrationUserData())
+                .chooseCountry()
+                .fillDateOfBirthDropdowns()
+                .markNewsletterCheckbox()
+                .markSpecialOfferCheckbox()
+                .clickCreateAccountButton()
+                .clickContinueButton()
+                .checkUserLoggedInText(loggedUser);
+    }
+}
