c6ad98cc3039ae3addcb7695761ffcda0f7b6fa9
Kofanskih <kofanskih08@gmail.com>
2025-08-23 17:06:03 +0300

    Updated HeaderPage, added clickLogo() method

--- ui-playwright/src/main/java/com/automationexercise/pages/HeaderPage.java
+++ ui-playwright/src/main/java/com/automationexercise/pages/HeaderPage.java
@@ -5,11 +5,18 @@ import com.microsoft.playwright.Page;
 import io.qameta.allure.Step;
 
 public class HeaderPage extends BasePage{
+    private final Locator LOGO = page.locator("//div[@class=\"logo pull-left\"]");
     private final Locator LOGIN_BUTTON = page.locator("text=Signup / Login");
 
 
     public HeaderPage(Page page) {
         super(page);
+    }
+
+    @Step("Click the logo button on the header")
+    public MainPage clickLogo(){
+        LOGO.click();
+        return new MainPage(page);
     }
 
     @Step("User click login button")
