dd24178ae0672e9026e12fab0192ab76fcf0d7c7
Kofanskih <kofanskih08@gmail.com>
2025-08-24 16:41:56 +0300

    Updated DeletePage, added checkTitleAfterDeletingAccount() method

--- ui-playwright/src/main/java/com/automationexercise/pages/DeletePage.java
+++ ui-playwright/src/main/java/com/automationexercise/pages/DeletePage.java
@@ -4,8 +4,11 @@ import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import io.qameta.allure.Step;
 
+import static org.testng.Assert.assertEquals;
+
 public class DeletePage extends BasePage{
     private final Locator CONTINUE_BUTTON = page.locator("[data-qa='continue-button']");
+    private final Locator ACCOUNT_DELETED_TITLE = page.locator("[data-qa='account-deleted']");
 
     public DeletePage(Page page) {
         super(page);
@@ -15,5 +18,11 @@ public class DeletePage extends BasePage{
     public MainPage clickContinueButtonOnTheDeletePage(){
         CONTINUE_BUTTON.click();
         return new MainPage(page);
+    }
+
+    @Step("Check the title after deleting account")
+    public void checkTitleAfterDeletingAccount(String title){
+        String actualText = ACCOUNT_DELETED_TITLE.innerText();
+        assertEquals(actualText, title);
     }
 }
