5631cbbf32467f9a086f19a97e4ae9352b3e0334
Kofanskih <kofanskih08@gmail.com>
2025-08-24 16:20:34 +0300

    Updated CheckoutPage, added goToTheLoginPage() test

--- ui-playwright/src/main/java/com/automationexercise/pages/CheckoutPage.java
+++ ui-playwright/src/main/java/com/automationexercise/pages/CheckoutPage.java
@@ -1,10 +1,12 @@
 package com.automationexercise.pages;
 
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import io.qameta.allure.Step;
 import org.testng.Assert;
 
 public class CheckoutPage extends BasePage{
+    private final Locator REGISTER_LOGIN_BUTTON = page.locator("a[href='/login']").nth(1);
 
     public CheckoutPage(Page page) {
         super(page);
@@ -14,5 +16,11 @@ public class CheckoutPage extends BasePage{
     public void checkUrlOnTheCheckoutPage(String expectedUrl) {
         String actualUrl = page.url();
         Assert.assertEquals(actualUrl, expectedUrl);
+    }
+
+    @Step("Go to The Login page")
+    public LoginPage goToTheLoginPage(){
+        REGISTER_LOGIN_BUTTON.click();
+        return new LoginPage(page);
     }
 }
