dc032205679f559efd809f310f6d3f1b46fe6d08
Kofanskih <kofanskih08@gmail.com>
2025-08-24 15:47:02 +0300

    Updated CartPage, added checkTheCartIsEmpty() method

--- ui-playwright/src/main/java/com/automationexercise/pages/CartPage.java
+++ ui-playwright/src/main/java/com/automationexercise/pages/CartPage.java
@@ -5,9 +5,11 @@ import com.microsoft.playwright.Page;
 import io.qameta.allure.Step;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class CartPage extends BasePage{
     private final Locator TITLE = page.locator("li.active");
+    private final Locator EMPTY_TITLE = page.locator("#empty_cart");
 
     public CartPage(Page page) {
         super(page);
@@ -17,5 +19,12 @@ public class CartPage extends BasePage{
     public void checkUserOnTheCartPage(String title) {
         String actualText = TITLE.innerText();
         assertEquals(actualText, title);
+    }
+
+    @Step("Check that cart is empty")
+    public void checkTheCartIsEmpty(String title) {
+        String actualText = EMPTY_TITLE.innerText();
+        assertTrue("Expected text to contain: '" + title + "' but was: '" + actualText + "'",
+                actualText.contains(title));
     }
 }
