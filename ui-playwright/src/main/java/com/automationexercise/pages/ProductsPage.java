16080bc8636d2767ded4f61b62438f94e89ea490
Kofanskih <kofanskih08@gmail.com>
2025-08-24 13:42:34 +0300

    Updated ProductsPage, added searchItemOnTheProductsPage(), checkSearchedItemOnTheProductsPage() methods

--- ui-playwright/src/main/java/com/automationexercise/pages/ProductsPage.java
+++ ui-playwright/src/main/java/com/automationexercise/pages/ProductsPage.java
@@ -8,6 +8,9 @@ import static org.testng.Assert.assertEquals;
 
 public class ProductsPage extends BasePage{
     private final Locator CENTER_TEXT_TITLE = page.locator(".title.text-center");
+    private final Locator SEARCH_FIELD = page.locator("#search_product");
+    private final Locator SEARCH_BUTTON = page.locator("#submit_search");
+    private final Locator SEARCH_RESULT = page.getByText("Winter Top").first();
 
     public ProductsPage(Page page) {
         super(page);
@@ -17,5 +20,18 @@ public class ProductsPage extends BasePage{
     public void checkTitleOnTheProductPage(String title) {
         String actualText = CENTER_TEXT_TITLE.innerText();
         assertEquals(actualText, title);
+    }
+
+    @Step("Search item on the Products page")
+    public ProductsPage searchItemOnTheProductsPage(String searchedItem){
+        SEARCH_FIELD.fill(searchedItem);
+        SEARCH_BUTTON.click();
+        return this;
+    }
+
+    @Step("Check searched item on the Products page")
+    public void checkSearchedItemOnTheProductsPage(String searchedItem){
+        String actualText = SEARCH_RESULT.innerText();
+        assertEquals(actualText, searchedItem);
     }
 }
