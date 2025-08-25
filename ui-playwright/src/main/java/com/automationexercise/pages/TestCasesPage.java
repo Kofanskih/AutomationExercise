ec6d0a9de78f3f63eb0f3b4ecec1ae46bb862a68
Kofanskih <kofanskih08@gmail.com>
2025-08-25 23:28:12 +0300

    Updated TestCasesPage, added collapseExpandedList(), checkTheListIsCollapsed() methods

--- ui-playwright/src/main/java/com/automationexercise/pages/TestCasesPage.java
+++ ui-playwright/src/main/java/com/automationexercise/pages/TestCasesPage.java
@@ -11,6 +11,7 @@ public class TestCasesPage extends BasePage{
     private final Locator CENTER_TEXT_TITLE = page.locator(".title.text-center");
     private final Locator LIST = page.locator("a[href='#collapse1']");
     private final Locator EXPANDED_BLOCK = page.locator("div[id='collapse1'] ul[class='list-group']");
+    private final Locator COLLAPSED_BLOCK = page.locator("div#collapse1.panel-collapse.collapsing");
 
     public TestCasesPage(Page page) {
         super(page);
@@ -31,5 +32,17 @@ public class TestCasesPage extends BasePage{
     @Step("Check the list is expanded")
     public void checkTheListIsExpanded(){
         assertTrue(EXPANDED_BLOCK.isVisible(), "Block should be expanded but is not visible");
+    }
+
+    @Step("Collapse the expanded list")
+    public TestCasesPage collapseExpandedList(){
+        EXPANDED_BLOCK.isVisible();
+        LIST.click();
+        return this;
+    }
+
+    @Step("Check the list is collapsed")
+    public void checkTheListIsCollapsed(){
+        assertTrue(COLLAPSED_BLOCK.isVisible(), "Block should be collapsed but is visible");
     }
 }
