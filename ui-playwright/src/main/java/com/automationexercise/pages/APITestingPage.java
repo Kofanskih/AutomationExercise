1a0999b66b17ce6a42de898e31707563652a4081
Anastasiia Bukovtseva <44721588+Kofanskih@users.noreply.github.com>
2025-08-25 22:39:36 +0300

    Api (#63)
    * Updated HeaderPage, added clickAPITestingButton() method
    
    * Added APITestingTests, added goToTheAPITestingPage() test
    
    * Updated APITestingPage, added expandCollapsedList(), checkTheListIsExpanded() methods
    
    * Updated APITestingTests, added expandListOnTheAPITestingPage() test
    
    * Updated APITestingPage, added collapseExpandedList(), checkTheListIsCollapsed() methods
    
    * Updated APITestingTests, added collapseExpandedListOnTheAPITestingPage() methods

--- ui-playwright/src/main/java/com/automationexercise/pages/APITestingPage.java
+++ ui-playwright/src/main/java/com/automationexercise/pages/APITestingPage.java
@@ -4,10 +4,13 @@ import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import io.qameta.allure.Step;
 
-import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.*;
 
 public class APITestingPage extends BasePage{
     private final Locator CENTER_TEXT_TITLE = page.locator(".title.text-center");
+    private final Locator LIST = page.locator("a[href='#collapse1']");
+    private final Locator EXPANDED_BLOCK = page.locator("div[id='collapse1'] ul[class='list-group']");
+    private final Locator COLLAPSED_BLOCK = page.locator("div#collapse.panel-collapse.collapsing");
 
     public APITestingPage(Page page) {
         super(page);
@@ -17,5 +20,28 @@ public class APITestingPage extends BasePage{
     public void checkTitleOnTheAPITestingPage(String title){
         String actualText = CENTER_TEXT_TITLE.innerText();
         assertEquals(actualText, title);
+    }
+
+    @Step("Expand the collapsed list")
+    public APITestingPage expandCollapsedList(){
+        LIST.click();
+        return this;
+    }
+
+    @Step("Check the list is expanded")
+    public void checkTheListIsExpanded(){
+        assertTrue(EXPANDED_BLOCK.isVisible(), "Block should be expanded but is not visible");
+    }
+
+    @Step("Collapse the expanded list")
+    public APITestingPage collapseExpandedList(){
+        EXPANDED_BLOCK.isVisible();
+        LIST.click();
+        return this;
+    }
+
+    @Step("Check the list is collapsed")
+    public void checkTheListIsCollapsed(){
+        COLLAPSED_BLOCK.isVisible();
     }
 }
---
+++ ui-playwright/src/test/java/com/automationexercise/ui/tests/APITestingTests.java
@@ -0,0 +1,46 @@
+package com.automationexercise.ui.tests;
+
+import com.automationexercise.pages.HeaderPage;
+import com.automationexercise.pages.MainPage;
+import io.qameta.allure.*;
+import org.testng.annotations.Test;
+
+@Epic("Test cases")
+@Feature("Api test cases")
+@Story("Test cases for API")
+@Severity(SeverityLevel.MINOR)
+@Owner("bukovtseva")
+@TmsLink("TC-009")
+public class APITestingTests extends BaseTest{
+    private String APITestingTitle = "APIS LIST FOR PRACTICE";
+
+    @Test(description = "User go to the API Testing page")
+    void goToTheAPITestingPage(){
+        new MainPage(page)
+                .acceptCookies();
+        new HeaderPage(page)
+                .clickAPITestingButton()
+                .checkTitleOnTheAPITestingPage(APITestingTitle);
+    }
+
+    @Test(description = "Expand list on the API Testing page")
+    void expandListOnTheAPITestingPage(){
+        new MainPage(page)
+                .acceptCookies();
+        new HeaderPage(page)
+                .clickAPITestingButton()
+                .expandCollapsedList()
+                .checkTheListIsExpanded();
+    }
+
+    @Test(description = "Collapse expanded list on the API Testing page")
+    void collapseExpandedListOnTheAPITestingPage(){
+        new MainPage(page)
+                .acceptCookies();
+        new HeaderPage(page)
+                .clickAPITestingButton()
+                .expandCollapsedList()
+                .collapseExpandedList()
+                .checkTheListIsCollapsed();
+    }
+}
