80d600f4a495ab73eaea56a6dcda904ec33f4aa0
Anastasiia Bukovtseva <44721588+Kofanskih@users.noreply.github.com>
2025-08-26 16:25:02 +0300

    Payment (#67)
    * Added PaymentPageModel, added existPaymentData() method
    
    * Updated PaymentPage, added fillPaymentForm(), checkPlaceOrderTitle() methods
    
    * Added PaymentTests, added payAndConfirmOrderWithCorrectData() test
    
    * Updated PaymentPage, added fillPaymentFormWithEmptyName(), getValidationMessage(), checkShowValidationMessageWhenNameOnCardIsEmpty() methods
    
    * Updated PaymentTests, added verifyInfoMessageWhenNameOnCardIsEmpty() test
    
    * Updated PaymentPage, added fillPaymentFormWithEmptyCardNumber(), checkShowValidationMessageWhenCardNumberIsEmpty() methods
    
    * Updated PaymentTests, added verifyInfoMessageWhenCardNumberIsEmpty() test
    
    * Updated PaymentPage, added fillPaymentFormWithEmptyCVC(), checkShowValidationMessageWhenCVCIsEmpty() methods
    
    * Updated PaymentTests, added verifyInfoMessageWhenCVCIsEmpty() test
    
    * Updated PaymentPage, added fillPaymentFormWithEmptyMonth(), checkShowValidationMessageWhenMonthIsEmpty() methods
    
    * Updated PaymentTests, added verifyInfoMessageWhenMonthIsEmpty() test
    
    * Updated PaymentPage, added fillPaymentFormWithEmptyYear(), checkShowValidationMessageWhenYearIsEmpty() methods
    
    * Updated PaymentTest, added verifyInfoMessageWhenYearIsEmpty() test
    
    * Updated PaymentPage, added clickContinueButton() method
    
    * Updated PaymentTes
    , added verifyUserIsOnTheMainPageAfterFinishPayment() test

--- ui-playwright/src/main/java/com/automationexercise/pages/PaymentPage.java
+++ ui-playwright/src/main/java/com/automationexercise/pages/PaymentPage.java
@@ -1,11 +1,23 @@
 package com.automationexercise.pages;
 
+import com.automationexercise.pageModels.PaymentPageModel;
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import io.qameta.allure.Step;
+import org.testng.AssertJUnit;
 
 import static org.testng.Assert.assertEquals;
 
 public class PaymentPage extends BasePage{
+    private final Locator NAME_ON_CARD_FIELD = page.locator("[name='name_on_card']");
+    private final Locator CARD_NUMBER_FIELD = page.locator("[data-qa='card-number']");
+    private final Locator CVC_FIELD = page.locator("[name='cvc']");
+    private final Locator EXPIRY_MONTH_FIELD = page.locator("[name='expiry_month']");
+    private final Locator EXPIRY_YEAR_FIELD = page.locator("[data-qa='expiry-year']");
+    private final Locator PAY_AND_CONFIRM_BUTTON = page.locator("[data-qa='pay-button']");
+    private final Locator TITLE = page.locator("[data-qa='order-placed']");
+    private final Locator CONTINUE_BUTTON = page.locator("[data-qa='continue-button']");
+
 
     public PaymentPage(Page page) {
         super(page);
@@ -15,5 +27,117 @@ public class PaymentPage extends BasePage{
     public void checkUrlOnThePaymentPage(String expectedUrl){
         String actualUrl = page.url();
         assertEquals(actualUrl, expectedUrl);
+    }
+
+    @Step("Fill payment form")
+    public PaymentPage fillPaymentForm(PaymentPageModel paymentPageModel){
+        NAME_ON_CARD_FIELD.fill(paymentPageModel.getNameOnCard());
+        CARD_NUMBER_FIELD.fill(paymentPageModel.getCardNumber());
+        CVC_FIELD.fill(paymentPageModel.getCvc());
+        EXPIRY_MONTH_FIELD.fill(paymentPageModel.getMonth());
+        EXPIRY_YEAR_FIELD.fill(paymentPageModel.getYear());
+        PAY_AND_CONFIRM_BUTTON.click();
+        return this;
+    }
+
+    @Step("Check place order title")
+    public void checkPlaceOrderTitle(String title) {
+        String actualText = TITLE.innerText();
+        assertEquals(actualText, title);
+    }
+
+    @Step("Fill payment form with empty name on card")
+    public PaymentPage fillPaymentFormWithEmptyName(PaymentPageModel paymentPageModel){
+        CARD_NUMBER_FIELD.fill(paymentPageModel.getCardNumber());
+        CVC_FIELD.fill(paymentPageModel.getCvc());
+        EXPIRY_MONTH_FIELD.fill(paymentPageModel.getMonth());
+        EXPIRY_YEAR_FIELD.fill(paymentPageModel.getYear());
+        PAY_AND_CONFIRM_BUTTON.click();
+        return this;
+    }
+
+    public String getValidationMessage(Locator element) {
+        return (String) element.evaluate("el => el.validationMessage");
+    }
+
+    @Step("Check validation message if name on card is empty")
+    public void checkShowValidationMessageWhenNameOnCardIsEmpty(String expectedMessage) {
+        Locator field = NAME_ON_CARD_FIELD;
+        String actualMessage = getValidationMessage(field);
+        AssertJUnit.assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Step("Fill payment form with empty card number")
+    public PaymentPage fillPaymentFormWithEmptyCardNumber(PaymentPageModel paymentPageModel){
+        NAME_ON_CARD_FIELD.fill(paymentPageModel.getNameOnCard());
+        CVC_FIELD.fill(paymentPageModel.getCvc());
+        EXPIRY_MONTH_FIELD.fill(paymentPageModel.getMonth());
+        EXPIRY_YEAR_FIELD.fill(paymentPageModel.getYear());
+        PAY_AND_CONFIRM_BUTTON.click();
+        return this;
+    }
+
+    @Step("Check validation message if card name is empty")
+    public void checkShowValidationMessageWhenCardNumberIsEmpty(String expectedMessage) {
+        Locator field = CARD_NUMBER_FIELD;
+        String actualMessage = getValidationMessage(field);
+        AssertJUnit.assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Step("Fill payment form with empty cvc")
+    public PaymentPage fillPaymentFormWithEmptyCVC(PaymentPageModel paymentPageModel){
+        NAME_ON_CARD_FIELD.fill(paymentPageModel.getNameOnCard());
+        CARD_NUMBER_FIELD.fill(paymentPageModel.getCardNumber());
+        EXPIRY_MONTH_FIELD.fill(paymentPageModel.getMonth());
+        EXPIRY_YEAR_FIELD.fill(paymentPageModel.getYear());
+        PAY_AND_CONFIRM_BUTTON.click();
+        return this;
+    }
+
+    @Step("Check validation message if cvc is empty")
+    public void checkShowValidationMessageWhenCVCIsEmpty(String expectedMessage) {
+        Locator field = CVC_FIELD;
+        String actualMessage = getValidationMessage(field);
+        AssertJUnit.assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Step("Fill payment form with empty month")
+    public PaymentPage fillPaymentFormWithEmptyMonth(PaymentPageModel paymentPageModel){
+        NAME_ON_CARD_FIELD.fill(paymentPageModel.getNameOnCard());
+        CARD_NUMBER_FIELD.fill(paymentPageModel.getCardNumber());
+        CVC_FIELD.fill(paymentPageModel.getCvc());
+        EXPIRY_YEAR_FIELD.fill(paymentPageModel.getYear());
+        PAY_AND_CONFIRM_BUTTON.click();
+        return this;
+    }
+
+    @Step("Check validation message if month is empty")
+    public void checkShowValidationMessageWhenMonthIsEmpty(String expectedMessage) {
+        Locator field = EXPIRY_MONTH_FIELD;
+        String actualMessage = getValidationMessage(field);
+        AssertJUnit.assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Step("Fill payment form with empty year")
+    public PaymentPage fillPaymentFormWithEmptyYear(PaymentPageModel paymentPageModel){
+        NAME_ON_CARD_FIELD.fill(paymentPageModel.getNameOnCard());
+        CARD_NUMBER_FIELD.fill(paymentPageModel.getCardNumber());
+        CVC_FIELD.fill(paymentPageModel.getCvc());
+        EXPIRY_MONTH_FIELD.fill(paymentPageModel.getMonth());
+        PAY_AND_CONFIRM_BUTTON.click();
+        return this;
+    }
+
+    @Step("Check validation message if year is empty")
+    public void checkShowValidationMessageWhenYearIsEmpty(String expectedMessage) {
+        Locator field = EXPIRY_YEAR_FIELD;
+        String actualMessage = getValidationMessage(field);
+        AssertJUnit.assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Step("Click continue button")
+    public MainPage clickContinueButton(){
+        CONTINUE_BUTTON.click();
+        return new MainPage(page);
     }
 }
---
+++ ui-playwright/src/test/java/com/automationexercise/ui/tests/PaymentTests.java
@@ -0,0 +1,140 @@
+package com.automationexercise.ui.tests;
+
+import com.automationexercise.pageModels.LoginPageModel;
+import com.automationexercise.pageModels.PaymentPageModel;
+import com.automationexercise.pages.HeaderPage;
+import com.automationexercise.pages.MainPage;
+import io.qameta.allure.*;
+import org.testng.annotations.Test;
+
+@Epic("Complete order")
+@Feature("Payment")
+@Story("Payment")
+@Severity(SeverityLevel.BLOCKER)
+@Owner("bukovtseva")
+@TmsLink("TC-013")
+public class PaymentTests extends BaseTest{
+    private String title = "ORDER PLACED!";
+    private String emptyInfoMessage = "Заполните это поле.";
+    private String expectedMainURL = "https://automationexercise.com/";
+
+    @Test(description = "Pay and confirm order with correct data")
+    void payAndConfirmOrderWithCorrectData() {
+        new MainPage(page)
+                .acceptCookies();
+        new HeaderPage(page)
+                .clickLoginButton()
+                .fillLoginForm(new LoginPageModel().myLogin());
+        new HeaderPage(page)
+                .clickProductsButton()
+                .addItemToTheCartOnTheProductsPage()
+                .clickOnTheViewCartButtonOnTheModalWindow()
+                .clickToCheckoutButtonOnTheCartPage()
+                .clickPlaceOrderButton()
+                .fillPaymentForm(new PaymentPageModel().existPaymentData())
+                .checkPlaceOrderTitle(title);
+    }
+
+    @Test(description = "Verify info message when name on card is empty")
+    void verifyInfoMessageWhenNameOnCardIsEmpty() {
+        new MainPage(page)
+                .acceptCookies();
+        new HeaderPage(page)
+                .clickLoginButton()
+                .fillLoginForm(new LoginPageModel().myLogin());
+        new HeaderPage(page)
+                .clickProductsButton()
+                .addItemToTheCartOnTheProductsPage()
+                .clickOnTheViewCartButtonOnTheModalWindow()
+                .clickToCheckoutButtonOnTheCartPage()
+                .clickPlaceOrderButton()
+                .fillPaymentFormWithEmptyName(new PaymentPageModel().existPaymentData())
+                .checkShowValidationMessageWhenNameOnCardIsEmpty(emptyInfoMessage);
+    }
+
+    @Test(description = "Verify info message when card number is empty")
+    void verifyInfoMessageWhenCardNumberIsEmpty() {
+        new MainPage(page)
+                .acceptCookies();
+        new HeaderPage(page)
+                .clickLoginButton()
+                .fillLoginForm(new LoginPageModel().myLogin());
+        new HeaderPage(page)
+                .clickProductsButton()
+                .addItemToTheCartOnTheProductsPage()
+                .clickOnTheViewCartButtonOnTheModalWindow()
+                .clickToCheckoutButtonOnTheCartPage()
+                .clickPlaceOrderButton()
+                .fillPaymentFormWithEmptyCardNumber(new PaymentPageModel().existPaymentData())
+                .checkShowValidationMessageWhenCardNumberIsEmpty(emptyInfoMessage);
+    }
+
+    @Test(description = "Verify info message when cvc is empty")
+    void verifyInfoMessageWhenCVCIsEmpty() {
+        new MainPage(page)
+                .acceptCookies();
+        new HeaderPage(page)
+                .clickLoginButton()
+                .fillLoginForm(new LoginPageModel().myLogin());
+        new HeaderPage(page)
+                .clickProductsButton()
+                .addItemToTheCartOnTheProductsPage()
+                .clickOnTheViewCartButtonOnTheModalWindow()
+                .clickToCheckoutButtonOnTheCartPage()
+                .clickPlaceOrderButton()
+                .fillPaymentFormWithEmptyCVC(new PaymentPageModel().existPaymentData())
+                .checkShowValidationMessageWhenCVCIsEmpty(emptyInfoMessage);
+    }
+
+    @Test(description = "Verify info message when month is empty")
+    void verifyInfoMessageWhenMonthIsEmpty() {
+        new MainPage(page)
+                .acceptCookies();
+        new HeaderPage(page)
+                .clickLoginButton()
+                .fillLoginForm(new LoginPageModel().myLogin());
+        new HeaderPage(page)
+                .clickProductsButton()
+                .addItemToTheCartOnTheProductsPage()
+                .clickOnTheViewCartButtonOnTheModalWindow()
+                .clickToCheckoutButtonOnTheCartPage()
+                .clickPlaceOrderButton()
+                .fillPaymentFormWithEmptyMonth(new PaymentPageModel().existPaymentData())
+                .checkShowValidationMessageWhenMonthIsEmpty(emptyInfoMessage);
+    }
+
+    @Test(description = "Verify info message when year is empty")
+    void verifyInfoMessageWhenYearIsEmpty() {
+        new MainPage(page)
+                .acceptCookies();
+        new HeaderPage(page)
+                .clickLoginButton()
+                .fillLoginForm(new LoginPageModel().myLogin());
+        new HeaderPage(page)
+                .clickProductsButton()
+                .addItemToTheCartOnTheProductsPage()
+                .clickOnTheViewCartButtonOnTheModalWindow()
+                .clickToCheckoutButtonOnTheCartPage()
+                .clickPlaceOrderButton()
+                .fillPaymentFormWithEmptyYear(new PaymentPageModel().existPaymentData())
+                .checkShowValidationMessageWhenYearIsEmpty(emptyInfoMessage);
+    }
+
+    @Test(description = "Verify user is on the main page after finish payment")
+    void verifyUserIsOnTheMainPageAfterFinishPayment() {
+        new MainPage(page)
+                .acceptCookies();
+        new HeaderPage(page)
+                .clickLoginButton()
+                .fillLoginForm(new LoginPageModel().myLogin());
+        new HeaderPage(page)
+                .clickProductsButton()
+                .addItemToTheCartOnTheProductsPage()
+                .clickOnTheViewCartButtonOnTheModalWindow()
+                .clickToCheckoutButtonOnTheCartPage()
+                .clickPlaceOrderButton()
+                .fillPaymentForm(new PaymentPageModel().existPaymentData())
+                .clickContinueButton()
+                .checkUrlOnTheMainPage(expectedMainURL);
+    }
+}
