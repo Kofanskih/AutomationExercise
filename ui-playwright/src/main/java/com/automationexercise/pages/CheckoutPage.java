677dc733f8754690a612831c0adfe5b87b34a24b
Kofanskih <kofanskih08@gmail.com>
2025-08-26 14:50:53 +0300

    Updated CheckoutPage, added clickPlaceOrderButton() method

--- ui-playwright/src/main/java/com/automationexercise/pages/CheckoutPage.java
+++ ui-playwright/src/main/java/com/automationexercise/pages/CheckoutPage.java
@@ -7,6 +7,7 @@ import org.testng.Assert;
 
 public class CheckoutPage extends BasePage{
     private final Locator REGISTER_LOGIN_BUTTON = page.locator("a[href='/login']").nth(1);
+    private final Locator PLACE_ORDER_BUTTON = page.locator(".btn.btn-default.check_out");
 
     public CheckoutPage(Page page) {
         super(page);
@@ -22,5 +23,11 @@ public class CheckoutPage extends BasePage{
     public LoginPage goToTheLoginPage(){
         REGISTER_LOGIN_BUTTON.click();
         return new LoginPage(page);
+    }
+
+    @Step("Click Place order button")
+    public PaymentPage clickPlaceOrderButton(){
+        PLACE_ORDER_BUTTON.click();
+        return new PaymentPage(page);
     }
 }
