0e91810516abd64d5b66cbb432850bf3614dc216
Kofanskih <kofanskih08@gmail.com>
2026-05-23 16:52:50 +0300

    Updated DriverManager

--- ui-selenium/src/main/java/com/automationexercise/utils/DriverManager.java
+++ ui-selenium/src/main/java/com/automationexercise/utils/DriverManager.java
@@ -1,4 +1,24 @@
 package com.automationexercise.utils;
 
+import org.openqa.selenium.WebDriver;
+
 public class DriverManager {
+    private static final ThreadLocal<WebDriver> threadLocalDriver = new ThreadLocal<>();
+
+    private DriverManager(){}
+
+    public static WebDriver getDriver(){
+        return threadLocalDriver.get();
+    }
+
+    public static void setDriver(WebDriver driver){
+        threadLocalDriver.set(driver);
+    }
+
+    public static void quitDriver(){
+        if(threadLocalDriver != null){
+            threadLocalDriver.get().quit();
+            threadLocalDriver.remove();
+        }
+    }
 }
